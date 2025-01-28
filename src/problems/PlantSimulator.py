ff7bd746b5b63f0fbcc25a3c92afbd5dc6ef9216
Anffany Chen <anffany@ualberta.ca>
2025-01-28 10:39:52 -0700

    incorporate night-time in PlantSimulator env, test out simple agents

--- src/problems/PlantSimulator.py
+++ src/problems/PlantSimulator.py
@@ -7,7 +7,7 @@ class PlantSimulator(BaseProblem):
     def __init__(self, exp: ExperimentModel, idx: int, collector: Collector):
         super().__init__(exp, idx, collector)
 
-        self.env = Env()
+        self.env = Env()  # default is two actions: off and on
         self.actions = 2
         self.observations = (2,)
         self.gamma = 0.99
