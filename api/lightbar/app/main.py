472c028bb9c13d1ef4fcc888c34b78656778fb61
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-12 21:18:11 +0000

    Put addresses into config file

--- api/lightbar/app/main.py
+++ api/lightbar/app/main.py
@@ -1,3 +1,4 @@
+import json
 from typing import List
 
 import numpy as np
@@ -6,8 +7,10 @@ from pydantic import BaseModel
 from typing_extensions import Annotated
 
 from .lightbar import Lightbar
+from .zones import zone2
 
 app = FastAPI()
+zone = zone2
 
 
 class Action(BaseModel):
@@ -15,16 +18,17 @@ class Action(BaseModel):
 
 
 def get_lightbar_left():
-    return Lightbar(0x69)
+    return Lightbar(zone.left)
 
 
 def get_lightbar_right():
-    return Lightbar(0x71)
+    return Lightbar(zone.right)
 
 
 @app.put("/action/left", response_class=Response)
 def update_action_left(action: Action, lightbar: Annotated[any, Depends(get_lightbar_left)]):
     lightbar.step(np.array(action.array))
+
 
 @app.put("/action/right", response_class=Response)
 def update_action_right(action: Action, lightbar: Annotated[any, Depends(get_lightbar_right)]):
