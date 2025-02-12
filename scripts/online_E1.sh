29a25d991604baaf5a30fd148b70787a858f9d69
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-12 22:53:21 +0000

    Add binary action environment

--- scripts/online_E1.sh
+++ scripts/online_E1.sh
@@ -4,3 +4,5 @@
 set -e
 
 python src/main_real.py -e experiments/online/E1/P0/Random.json -i 0
+python src/main_real.py -e experiments/online/E1/P1/Random.json -i 0
+python src/main_real.py -e experiments/online/E1/P2/Random.json -i 0
