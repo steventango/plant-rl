4a0bc6fd09dd46707616db4d91fd9cc996254921
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-13 07:08:34 +0000

    Use local.py to gather missing indicies

--- scripts/online_E2.sh
+++ scripts/online_E2.sh
@@ -2,9 +2,6 @@
 
 # exit script on error
 set -e
-
-for i in 0 1 2; do
-  python src/main_real.py -e experiments/online/E2/P0/GAC.json -i $i
-  python src/main_real.py -e experiments/online/E2/P0/Random.json -i $i
-  python src/main_real.py -e experiments/online/E2/P0/Constant.json -i $i
-done
+python scripts/local.py --runs 3 --cpus 1 --entry src/main_real.py -e experiments/online/E2/P0/GAC.json
+python scripts/local.py --runs 3 --cpus 1 --entry src/main_real.py -e experiments/online/E2/P0/Random.json
+python scripts/local.py --runs 3 --cpus 1 --entry src/main_real.py -e experiments/online/E2/P0/Constant.json
