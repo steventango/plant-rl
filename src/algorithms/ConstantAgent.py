f7521716706ec908098693b7c1692a53613c322c
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-13 06:44:47 +0000

    Add constant agent

---
+++ src/algorithms/ConstantAgent.py
@@ -0,0 +1,26 @@
+from typing import Any, Dict, Tuple
+
+import numpy as np
+from PyExpUtils.collection.Collector import Collector
+
+from algorithms.BaseAgent import BaseAgent
+
+
+class ConstantAgent(BaseAgent):
+    def __init__(self, observations: Tuple[int, ...], actions: int, params: Dict, collector: Collector, seed: int):
+        super().__init__(observations, actions, params, collector, seed)
+        self.steps = 0
+        self.updates = 0
+        self.action = self.params["action"]
+
+    # ----------------------
+    # -- RLGlue interface --
+    # ----------------------
+    def start(self, observation: np.ndarray):
+        return self.action
+
+    def step(self, reward: float, observation: np.ndarray | None, extra: Dict[str, Any]):
+        return self.action
+
+    def end(self, reward: float, extra: Dict[str, Any]):
+        pass
