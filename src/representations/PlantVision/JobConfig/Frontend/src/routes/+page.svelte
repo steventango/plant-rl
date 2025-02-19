9974e8928ef45f5a7a93035c415be837f54b29a9
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-19 04:01:24 +0000

    Remove frontend

--- src/representations/PlantVision/JobConfig/Frontend/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<div class="flex items-center justify-center h-full">
-    <div class="grid grid-cols-1 gap-7">
-        <a href="/tools/timeframe" class="btn variant-filled-primary mx-3" data-sveltekit-preload-data="hover">Select Timeframe</a>
-        <a href="/tools/undistort-fisheye" class="btn variant-filled-primary mx-3" data-sveltekit-preload-data="hover">Undistort Fisheye</a>
-        <a href="/tools/threshold" class="btn variant-filled-primary mx-3" data-sveltekit-preload-data="hover">Threshold</a>
-        <a href="/tools/draw" class="btn variant-filled-primary mx-3" data-sveltekit-preload-data="hover">Draw</a>
-    </div>
-</div>
