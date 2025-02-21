204ae70017e227450331c5c5f76a92ea57f73158
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-21 16:19:26 -0700

    Use 10k total steps and manually tune hypers

--- scripts/offline_E2.sh
+++ scripts/offline_E2.sh
@@ -6,3 +6,4 @@ set -e
 python scripts/local.py --runs 1 -e experiments/offline/E2/P1/Constant.json
 python scripts/local.py --runs 1 -e experiments/offline/E2/P1/Random.json
 python scripts/local.py --runs 1 -e experiments/offline/E2/P1/GAC-sweep.json
+python experiments/offline/E2/compare-alg.py
