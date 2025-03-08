737090da379c1a6ea5b816a497985bae53a653f4
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-03-08 20:10:08 +0000

    Revert to full zone lightbar control

--- api/lightbar/tests/test_main.py
+++ api/lightbar/tests/test_main.py
@@ -1,44 +1,33 @@
 import numpy as np
 from fastapi.testclient import TestClient
 
-from ..app.main import app, get_lightbar_left, get_lightbar_right
+from ..app.main import app, get_lightbar
+from ..app.zones import Zone
 from .mock_lightbar import MockLightbar
-
-lightbar_left = MockLightbar(0x69)
-lightbar_right = MockLightbar(0x71)
+lightbar = MockLightbar(Zone(0x69, 0x71))
 
 
-def get_mock_lightbar_left():
-    return lightbar_left
+def get_mock_lightbar():
+    return lightbar
 
 
-def get_mock_lightbar_right():
-    return lightbar_right
-
-
-app.dependency_overrides[get_lightbar_left] = get_mock_lightbar_left
-app.dependency_overrides[get_lightbar_right] = get_mock_lightbar_right
+app.dependency_overrides[get_lightbar] = get_mock_lightbar
 
 client = TestClient(app)
 
 
-def test_action_left():
-    response = client.put("/action/left", json={"array": np.ones(6).tolist()})
+def test_action():
+    response = client.put("/action", json={"array": np.ones((2, 6)).tolist()})
     assert response.status_code == 200
-    assert lightbar_left.i2c.data[0x69][3][0] == [0, 0x06, 0, 0, 0x55, 0x05]
-    assert lightbar_left.i2c.data[0x69][3][1] == [0, 0x0A, 0, 0, 0x55, 0x05]
-    assert lightbar_left.i2c.data[0x69][3][2] == [0, 0x1E, 0, 0, 0x55, 0x05]
-    assert lightbar_left.i2c.data[0x69][3][3] == [0, 0x1A, 0, 0, 0x55, 0x05]
-    assert lightbar_left.i2c.data[0x69][3][4] == [0, 0x16, 0, 0, 0x55, 0x05]
-    assert lightbar_left.i2c.data[0x69][3][5] == [0, 0x0E, 0, 0, 0x55, 0x05]
-
-
-def test_action_right():
-    response = client.put("/action/right", json={"array": np.ones(6).tolist()})
-    assert response.status_code == 200
-    assert lightbar_right.i2c.data[0x71][3][0] == [0, 0x06, 0, 0, 0x55, 0x05]
-    assert lightbar_right.i2c.data[0x71][3][1] == [0, 0x0A, 0, 0, 0x55, 0x05]
-    assert lightbar_right.i2c.data[0x71][3][2] == [0, 0x1E, 0, 0, 0x55, 0x05]
-    assert lightbar_right.i2c.data[0x71][3][3] == [0, 0x1A, 0, 0, 0x55, 0x05]
-    assert lightbar_right.i2c.data[0x71][3][4] == [0, 0x16, 0, 0, 0x55, 0x05]
-    assert lightbar_right.i2c.data[0x71][3][5] == [0, 0x0E, 0, 0, 0x55, 0x05]
+    assert lightbar.i2c.data[0x69][3][0] == [0, 0x06, 0, 0, 0x55, 0x05]
+    assert lightbar.i2c.data[0x69][3][1] == [0, 0x0A, 0, 0, 0x55, 0x05]
+    assert lightbar.i2c.data[0x69][3][2] == [0, 0x1E, 0, 0, 0x55, 0x05]
+    assert lightbar.i2c.data[0x69][3][3] == [0, 0x1A, 0, 0, 0x55, 0x05]
+    assert lightbar.i2c.data[0x69][3][4] == [0, 0x16, 0, 0, 0x55, 0x05]
+    assert lightbar.i2c.data[0x69][3][5] == [0, 0x0E, 0, 0, 0x55, 0x05]
+    assert lightbar.i2c.data[0x71][3][0] == [0, 0x06, 0, 0, 0x55, 0x05]
+    assert lightbar.i2c.data[0x71][3][1] == [0, 0x0A, 0, 0, 0x55, 0x05]
+    assert lightbar.i2c.data[0x71][3][2] == [0, 0x1E, 0, 0, 0x55, 0x05]
+    assert lightbar.i2c.data[0x71][3][3] == [0, 0x1A, 0, 0, 0x55, 0x05]
+    assert lightbar.i2c.data[0x71][3][4] == [0, 0x16, 0, 0, 0x55, 0x05]
+    assert lightbar.i2c.data[0x71][3][5] == [0, 0x0E, 0, 0, 0x55, 0x05]
