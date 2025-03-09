cd4e66319ef7ecdee9efdf41b00d6be11874dbad
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-03-09 05:11:22 +0000

    Update to use picamera

--- api/camera/app/main.py
+++ api/camera/app/main.py
@@ -1,29 +1,26 @@
 import io
-from functools import lru_cache
+import time
+from functools import cache
 from typing import Annotated
 
 from fastapi import Depends, FastAPI, Response
-from PIL import Image
 
 app = FastAPI()
 
 
-@lru_cache(maxsize=None)
+@cache
 def get_camera():
-    from picamera2 import Picamera2
-    camera = Picamera2()
-    camera.configure("still")
-    camera.start()
+    from picamera import PiCamera
+    camera = PiCamera()
+    camera.start_preview()
+    time.sleep(2)
     return camera
 
 
 @app.get("/observation", response_class=Response)
 def read_observation(cam: Annotated[any, Depends(get_camera)]):
-    array = cam.capture_array()
-    image = Image.fromarray(array)
-
     with io.BytesIO() as buf:
-        image.save(buf, format="PNG")
+        cam.capture(buf, format='png')
         image_bytes = buf.getvalue()
 
     headers = {"Content-Disposition": 'inline; filename="observation.png"'}
