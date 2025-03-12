ce4bb9782074997e2cb7f414bfd08afed88d17c3
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-03-12 16:15:51 -0600

    Patch httpx

--- api/status/tests/test_main.py
+++ api/status/tests/test_main.py
@@ -10,7 +10,7 @@ client = TestClient(app)
 
 
 def test_homepage():
-    with patch("requests.get") as mock_get:
+    with patch("httpx.AsyncClient.get") as mock_get:
         mock_get.return_value = MagicMock(status_code=200, json=lambda: None)
         response = client.get("/")
         assert response.status_code == 200
