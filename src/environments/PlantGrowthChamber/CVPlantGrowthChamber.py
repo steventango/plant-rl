4c87c10e76c6bb16a5a21ecdc398b35548c19b70
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-03-18 17:12:16 -0600

    Calculate reward function

--- src/environments/PlantGrowthChamber/CVPlantGrowthChamber.py
+++ src/environments/PlantGrowthChamber/CVPlantGrowthChamber.py
@@ -1,15 +1,12 @@
 import numpy as np
 
 from environments.PlantGrowthChamber.PlantGrowthChamber import PlantGrowthChamber
-from utils.metrics import UnbiasedExponentialMovingAverage
 
 
 class CVPlantGrowthChamber(PlantGrowthChamber):
     def __init__(self, zone: int, total_steps: int = 40320):
         super().__init__(zone)
         self.total_steps = total_steps
-        # todo don't hardcode 16
-        self.plant_area_emas = [UnbiasedExponentialMovingAverage(alpha=0.1)] * 16
 
     def get_observation(self):
         time, _, plant_stats = super().get_observation()
@@ -19,22 +16,12 @@ class CVPlantGrowthChamber(PlantGrowthChamber):
         # todo don't hardcode 5 minutes
         countdown = self.transform_time(time, self.total_steps * 5 * 60)
 
-        plant_area_emas_prev = [plant_area_ema.compute() for plant_area_ema in self.plant_area_emas]
-        mean_plant_area_ema_prev = np.mean(plant_area_emas_prev)
-
-        plant_areas = plant_stats[:, 0]
-        for plant_area_ema in self.plant_area_emas:
-            plant_area_ema.update(values=plant_areas)
-
-        plant_area_emas = [plant_area_ema.compute() for plant_area_ema in self.plant_area_emas]
-        mean_plant_area_ema = np.mean(plant_area_emas)
-
         observation = np.array(
             [
                 *transformed_time,
                 *countdown,
-                mean_plant_area_ema_prev,
-                mean_plant_area_ema,
+                self.mean_plant_area_ema_prev,
+                self.mean_plant_area_ema,
             ]
         )
         return observation
--- src/environments/PlantGrowthChamber/PlantGrowthChamber.py
+++ src/environments/PlantGrowthChamber/PlantGrowthChamber.py
@@ -5,9 +5,13 @@ from datetime import datetime
 import numpy as np
 import requests
 from PIL import Image
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
 
+from utils.metrics import UnbiasedExponentialMovingAverage
 from utils.RlGlue.environment import BaseAsyncEnvironment
 
+from .utils import process_image
 from .zones import get_zone
 
 
@@ -19,7 +23,15 @@ class PlantGrowthChamber(BaseAsyncEnvironment):
         self.images = {}
         self.image = None
         self.time = None
+        self.lag = 1
         self._start_time = start_time
+        # todo don't hardcode 16
+        self.plant_area_emas = [UnbiasedExponentialMovingAverage(alpha=0.1)] * 16
+        self.min_action = 0.35 * np.array([0.398, 0.762, 0.324, 0.000, 0.332, 0.606])
+        self.session = requests.Session()
+        retries = Retry(total=3, backoff_factor=1, status_forcelist=[500, 502, 503, 504])
+        self.session.mount("http://", HTTPAdapter(max_retries=retries))
+        self.session.mount("https://", HTTPAdapter(max_retries=retries))
 
     def get_observation(self):
         self.time = datetime.now().timestamp()
@@ -32,16 +44,26 @@ class PlantGrowthChamber(BaseAsyncEnvironment):
         elif "right" in self.images:
             self.image = np.array(self.images["right"])
 
-        self.processed_image = self.process_image(self.image)
+        self.df = process_image(self.image, self.zone.trays, self.images)
 
-        self.plant_stats = np.random.randn(16, 1)
+        self.plant_stats = np.array(self.df, dtype=np.float32)
+
+        plant_area_emas_prev = [plant_area_ema.compute() for plant_area_ema in self.plant_area_emas]
+        self.mean_plant_area_ema_prev = np.mean(plant_area_emas_prev)
+
+        plant_areas = self.plant_stats[:, 2]
+        for plant_area_ema in self.plant_area_emas:
+            plant_area_ema.update(values=plant_areas)
+
+        plant_area_emas = [plant_area_ema.compute() for plant_area_ema in self.plant_area_emas]
+        self.mean_plant_area_ema = np.mean(plant_area_emas)
 
         return self.time, self.image, self.plant_stats
 
     def get_image(self):
 
         def fetch_image(url: str):
-            response = requests.get(url, timeout=30)
+            response = self.session.get(url, timeout=60)
             response.raise_for_status()
             return Image.open(io.BytesIO(response.content))
 
@@ -54,39 +76,6 @@ class PlantGrowthChamber(BaseAsyncEnvironment):
 
             for side, future in futures.items():
                 self.images[side] = future.result()
-
-    def process_image(self, image: np.ndarray):
-        POT_WIDTH = 100
-        POT_HEIGHT = 100
-        # Get source points from the tray's rectangle
-        if self.zone.trays:
-            processed_images = []
-            for tray in self.zone.trays:
-                src_points = np.array(
-                    [tray.rect.top_left, tray.rect.top_right, tray.rect.bottom_right, tray.rect.bottom_left],
-                    dtype=np.float32,
-                )
-                # Calculate the height and width from pot widths and heights in each tray
-                width = tray.n_wide * POT_WIDTH
-                height = tray.n_tall * POT_HEIGHT
-
-                # Define destination points (desired corners of the tray)
-                dst_points = np.array(
-                    [[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]], dtype=np.float32
-                )
-                 # Compute the homography matrix
-                homography_matrix, _ = cv2.findHomography(src_points, dst_points)
-
-                # Warp the image using the homography matrix
-                processed_image = cv2.warpPerspective(image, homography_matrix, (width, height))
-
-                processed_images.append(processed_image)
-
-            processed_image = np.hstack(processed_images)
-        else:
-            processed_image = image
-
-        return processed_image
 
     def start(self):
         observation = self.get_observation()
@@ -101,15 +90,25 @@ class PlantGrowthChamber(BaseAsyncEnvironment):
         return self.reward, observation, False, self.get_info()
 
     def put_action(self, action):
+        # clip action to be between min_action and 1
+        action = np.clip(action, self.min_action, 1)
         action = np.tile(action, (2, 1))
-        response = requests.put(self.zone.lightbar_url, json={"array": action.tolist()}, timeout=5)
+        response = self.session.put(self.zone.lightbar_url, json={"array": action.tolist()}, timeout=10)
         response.raise_for_status()
 
     def get_info(self):
         return {"gamma": self.gamma}
 
     def reward_function(self):
-        return np.array(self.image).mean() / 255
+        new = self.normalize(self.mean_plant_area_ema)
+        old = self.normalize(self.mean_plant_area_ema_prev)
+        return (new / old - 1).item()
+
+    def normalize(self, x):  # normalize observation to between 0 and 1
+        # TODO: check if this number is too big?
+        u = 30000  # max historic area of one plant (in pixels)
+        l = 0
+        return (x - l) / (u - l)
 
     def close(self):
         requests.put(self.zone.lightbar_url, json={"array": np.zeros((2, 6)).tolist()})
