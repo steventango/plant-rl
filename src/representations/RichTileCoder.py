1e87b3d3abde5684503ecf44b1b9d525c8ed5db6
Anffany Chen <anffany@ualberta.ca>
2025-03-22 01:39:39 -0600

    experiment with different tile coding

--- src/representations/RichTileCoder.py
+++ src/representations/RichTileCoder.py
@@ -36,7 +36,7 @@ class RichTileCoder():
     def get_indices(self, s: np.ndarray):   
         if self._c.multi_call:   # This specifically works for the 3D state space (time, area, change in area)
             tile1 = tiles(self.iht, self._c.tilings, [s[0]*self.scale[0]], [0])   # 1D time
-            tile2 = tiles(self.iht, self._c.tilings, [s[1]*self.scale[1], s[2]*self.scale[2]], [1])   # 2D (area, change in area)
+            tile2 = tiles(self.iht, self._c.tilings, [s[0]*self.scale[0], s[1]*self.scale[1]], [1])   # 2D (time, area)
             tile3 = tiles(self.iht, self._c.tilings, [s[0]*self.scale[0], s[2]*self.scale[2]], [2])   # 2D (time, change in area)
             return tile1 + tile2 + tile3
         else:
@@ -49,10 +49,13 @@ class RichTileCoder():
         return num_tiles / abs(range[1] - range[0])
 
     def compute_maxSize(self, x):
-        a = self._c.tilings
-        for num_tiles in x: 
-            a *= num_tiles + 1
-        return a
+        if self._c.multi_call:     # This specifically works for the 3D state space (time, area, change in area)
+            return self._c.tilings * ((x[0] + 1) + (x[0] + 1) * (x[1] + 1) + (x[0] + 1) * (x[2] + 1))
+        else:
+            a = self._c.tilings
+            for num_tiles in x: 
+                a *= num_tiles + 1
+            return a
     
     def encode(self, s: np.ndarray):
         indices = self.get_indices(s)
