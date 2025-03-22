833a571d68f5df1974c69e0252d8941feeaed6a9
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-03-22 03:38:13 +0000

    Generate plots for current reward function

--- scripts/offline_E3.sh
+++ scripts/offline_E3.sh
@@ -3,4 +3,5 @@
 # exit script on error
 set -e
 
-python scripts/local.py --runs 5 --cpus 1 -e experiments/offline/E3/P5/tc-ESARSA.json 
+python scripts/local.py --runs 5 --cpus 5 -e experiments/offline/E3/P5/optimal-sequence.json
+python scripts/local.py --runs 5 --cpus 30 -e experiments/offline/E3/P5/tc-ESARSA.json
