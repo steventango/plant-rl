9f53ee13e574cff963c073648fda310eeff88757
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-03-25 21:30:52 +0000

    Add histogram matching

--- src/environments/PlantGrowthChamber/cv.py
+++ src/environments/PlantGrowthChamber/cv.py
@@ -1,12 +1,17 @@
 from collections import defaultdict
+from pathlib import Path
 
 import cv2
 import numpy as np
 import pandas as pd
 from PIL import Image
 from plantcv import plantcv as pcv
+from skimage.exposure import match_histograms
 
 from .zones import POT_HEIGHT, POT_WIDTH, SCALE, Tray
+
+reference = np.array(Image.open(Path(__file__).parent / "reference.png"))
+reference = cv2.cvtColor(reference, cv2.COLOR_RGB2LAB)
 
 
 def process_image(image: np.ndarray, trays: list[Tray], debug_images: dict[str, Image]):
@@ -17,6 +22,8 @@ def process_image(image: np.ndarray, trays: list[Tray], debug_images: dict[str, 
 
     camera_matrix = np.array([[1800.0, 0.0, 1296.0], [0.0, 1800.0, 972.0], [0.0, 0.0, 1.0]])
     dist_coeffs = np.array([0.0, 0.0, 0.0, 0.0])
+    # convert image from RGBA to RGB
+    image = cv2.cvtColor(image, cv2.COLOR_RGBA2RGB)
     undistorted_image = cv2.undistort(image, camera_matrix, dist_coeffs)
     debug_images["undistorted"] = Image.fromarray(undistorted_image)
     for tray in trays:
@@ -45,38 +52,47 @@ def process_tray(image: np.ndarray, tray: Tray, debug_images: dict[str, list[np.
     warped_image = cv2.warpPerspective(image, homography_matrix, (width, height))
     debug_images["warped"].append(warped_image)
 
-    MARGIN = 0.7
-    without_border_images = []
-    for i in range(tray.n_wide):
-        for j in range(tray.n_tall):
-            without_border_image = get_pot_crop(warped_image, i, j, MARGIN, POT_WIDTH)
-            without_border_images.append(without_border_image)
-    # combine without_border_images into one image (n_wide x n_tall)
-    without_border_images = np.array(without_border_images)
-    without_border_images = without_border_images.reshape(tray.n_tall, tray.n_wide, *without_border_images.shape[1:])
-    # reassemble images into one image
-    without_border_image = np.vstack([np.hstack(row) for row in without_border_images])
-    debug_images["without_border"].append(without_border_image)
+    # MARGIN = 0.7
+    # without_border_images = []
+    # for i in range(tray.n_wide):
+    #     for j in range(tray.n_tall):
+    #         without_border_image = get_pot_crop(warped_image, i, j, MARGIN, POT_WIDTH)
+    #         without_border_images.append(without_border_image)
+    # # combine without_border_images into one image (n_wide x n_tall)
+    # without_border_images = np.array(without_border_images)
+    # without_border_images = without_border_images.reshape(tray.n_tall, tray.n_wide, *without_border_images.shape[1:])
+    # # reassemble images into one image
+    # without_border_image = np.vstack([np.hstack(row) for row in without_border_images])
+    # debug_images["without_border"].append(without_border_image)
 
-    colorspaces = pcv.visualize.colorspaces(rgb_img=without_border_image, original_img=False)
+    lab = cv2.cvtColor(warped_image, cv2.COLOR_RGB2LAB)
+    matched = match_histograms(lab, reference, channel_axis=-1)
+    matched = cv2.cvtColor(matched, cv2.COLOR_LAB2RGB)
+    debug_images["matched"].append(matched)
+
+    colorspaces = pcv.visualize.colorspaces(rgb_img=matched, original_img=False)
     debug_images["colorspaces"].append(colorspaces)
-    gray_image = pcv.rgb2gray_lab(rgb_img=without_border_image, channel="a")
+
+    gray_image = pcv.rgb2gray_lab(rgb_img=matched, channel="a")
     debug_images["gray"].append(gray_image)
     normalized_gray_image = (gray_image - np.mean(gray_image)) / np.std(gray_image)
-    normalized_gray_image = 127 + 64 * normalized_gray_image
+    MEAN = 127
+    STD = 64
+    normalized_gray_image = MEAN + STD * normalized_gray_image
     normalized_gray_image = np.clip(normalized_gray_image, 0, 255).astype(np.uint8)
     debug_images["normalized_gray"].append(normalized_gray_image)
-    pot_width = int(POT_WIDTH * MARGIN)
-    mask = pcv.threshold.mean(gray_img=normalized_gray_image, ksize=3 * pot_width, offset=1.5 * 64, object_type="dark")
+    pot_width = int(POT_WIDTH)
+    OFFSET = 1.5
+    mask = pcv.threshold.mean(gray_img=normalized_gray_image, ksize=3 * pot_width, offset=OFFSET * STD, object_type="dark")
     debug_images["mask"].append(mask)
-    FILL_THRESHOLD = 0.005 * pot_width**2
+    FILL_THRESHOLD = 0.003 * pot_width**2
     mask = pcv.fill(mask, FILL_THRESHOLD)
     debug_images["mask_filled"].append(mask)
     debug_pot_images = defaultdict(list)
     stats = []
     for i in range(tray.n_wide):
         for j in range(tray.n_tall):
-            pot_image = get_pot_crop(without_border_image, i, j, 1, pot_width)
+            pot_image = get_pot_crop(matched, i, j, 1, pot_width)
             pot_mask = get_pot_crop(mask, i, j, 1, pot_width)
             shape_image, stat = process_plant(pot_image, pot_mask, debug_pot_images)
             stats.append(stat)
@@ -110,8 +126,7 @@ def process_plant(image: np.ndarray, mask, debug_images: dict[str, list[np.ndarr
     labeled_mask, num_plants = pcv.create_labels(mask=mask, rois=roi, roi_type="partial")
     from plantcv.plantcv import params
     params.line_thickness = 1
-    # convert image from RGBA to RGB
-    image = cv2.cvtColor(image, cv2.COLOR_RGBA2RGB)
+
     shape_image = pcv.analyze.size(img=image, labeled_mask=labeled_mask, n_labels=num_plants)
     shape_image = cv2.circle(shape_image, (x, y), r, (0, 255, 255), 1)
 
