60543540a4bd506a1f92c0fd55adcf6ced528e74
Anffany Chen <anffany@ualberta.ca>
2025-03-26 14:37:24 -0600

    edited get_observation in CVPlantGrowthChamber.py

--- src/environments/PlantGrowthChamber/CVPlantGrowthChamber.py
+++ src/environments/PlantGrowthChamber/CVPlantGrowthChamber.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 from environments.PlantGrowthChamber.PlantGrowthChamber import PlantGrowthChamber
+from utils.metrics import iqm
+from utils.metrics import UnbiasedExponentialMovingAverage as uema
 
 
 class CVPlantGrowthChamber(PlantGrowthChamber):
@@ -8,23 +10,33 @@ class CVPlantGrowthChamber(PlantGrowthChamber):
         super().__init__(zone)
         self.total_steps = total_steps
 
+        self.history = uema(alpha=0.01)   # history of change in average observed area over 1 time step (in units of mm^2)
+ 
     def get_observation(self):
         time, _, plant_stats = super().get_observation()
 
-        transformed_time = self.transform_time(time)
-
-        # todo don't hardcode 5 minutes
-        countdown = self.transform_time(time, self.total_steps * 5 * 60)
-
-        observation = np.array(
-            [
-                *transformed_time,
-                *countdown,
-                self.mean_plant_area_ema_prev,
-                self.mean_plant_area_ema,
-            ]
-        )
+        if len(self.observed_areas) >= 2:
+            self.history.update(iqm(self.observed_areas[-1], self.q) - iqm(self.observed_areas[-2], self.q))
+        
+        time_of_day = self.transform_time_linear(time)
+        
+        observation = np.hstack([time_of_day,
+                                 self.normalize(iqm(self.observed_areas[-1], self.q)),
+                                 self.normalize(self.history.compute(), l=-0.41, u=2.48)])
+        
         return observation
+    
+    def start(self):
+        self.history.reset() 
+        self.observed_areas = []
+        observation = self.get_observation()
+        return observation
+    
+    def reward_function(self):   # reward = last state input = smooth change in area
+        return self.normalize(self.history.compute().item(), l=-0.41, u=2.48)
 
-    def transform_time(self, time, total=86400):
+    def transform_time_sine(self, time, total=86400.0):
         return np.array([np.sin(2 * np.pi * time / total), np.cos(2 * np.pi * time / total)])
+
+    def transform_time_linear(self, time, total=86400.0):
+        return time / total
--- src/environments/PlantGrowthChamber/PlantGrowthChamber.py
+++ src/environments/PlantGrowthChamber/PlantGrowthChamber.py
@@ -7,9 +7,8 @@ import requests
 from PIL import Image
 from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
-
 from utils.metrics import iqm
-from utils.metrics import UnbiasedExponentialMovingAverage as uema
+
 from utils.RlGlue.environment import BaseAsyncEnvironment
 
 from .utils import process_image
@@ -29,13 +28,14 @@ class PlantGrowthChamber(BaseAsyncEnvironment):
         retries = Retry(total=3, backoff_factor=1, status_forcelist=[500, 502, 503, 504])
         self.session.mount("http://", HTTPAdapter(max_retries=retries))
         self.session.mount("https://", HTTPAdapter(max_retries=retries))
+        
+        # TODO: save self.observed_areas at the end of the run
 
-        self.q = 0.10                     # the bottom q and the top 1-q quantiles are excluded from iqm
         self.observed_areas = []          # stores a list of arrays of observed areas in mm^2. i.e. self.observed_areas[-1] contains the latest areas of individual plants
-        self.history = uema(alpha=0.01)   # history of change in average observed area over 1 time step (in units of mm^2). Note that tracing also happens at night.
+        self.q = 0.10                     # the bottom q and the top 1-q quantiles are excluded from iqm of areas
         self.gamma = 1.0
 
-    def get_raw_observation(self):
+    def get_observation(self):
         self.time = datetime.now().timestamp()
 
         self.get_image()
@@ -49,6 +49,11 @@ class PlantGrowthChamber(BaseAsyncEnvironment):
         self.df = process_image(self.image, self.zone.trays, self.images)
 
         self.plant_stats = np.array(self.df, dtype=np.float32)
+
+        plant_areas = self.plant_stats[:, 2].reshape(1, -1)  
+        self.observed_areas.append(plant_areas.flatten())
+
+        return self.time, self.image, self.plant_stats
 
     def get_image(self):
 
@@ -66,21 +71,6 @@ class PlantGrowthChamber(BaseAsyncEnvironment):
 
             for side, future in futures.items():
                 self.images[side] = future.result()
-    
-    def get_input_observation(self):
-        plant_areas = self.plant_stats[:, 2].reshape(1, -1)  # TODO: Steven, this is a 1D array right?
-        self.observed_areas.append(plant_areas.flatten())
-
-        if len(self.observed_areas) >= 2:
-            self.history.update(iqm(self.observed_areas[-1], self.q) - iqm(self.observed_areas[-2], self.q))
-        
-        time_of_day = self.transform_time_linear(self.time, total=86400/2) # time goes from 0 to 1 over 12hr period
-        
-        observation = np.hstack([time_of_day,
-                                 self.normalize(iqm(self.observed_areas[-1], self.q)),
-                                 self.normalize(self.history.compute(), l=-0.41, u=2.48)])
-        
-        return observation
 
     def step_one(self, action: np.ndarray):
         self.put_action(action)
@@ -94,18 +84,11 @@ class PlantGrowthChamber(BaseAsyncEnvironment):
 
     def start(self):
         self.observed_areas = []
-        self.history.reset()
-
-        self.get_raw_observation()
-        observation = self.get_input_observation()
+        observation = self.get_observation()
         return observation
 
     def step_two(self):
-        # TODO insert here a call for overnight behavior (tracing history)
-        # TODO if observed_areas include overnight measurements, need to revisit reward and history definitions
-
-        self.get_raw_observation()
-        observation = self.get_input_observation()
+        observation = self.get_observation()
         self.reward = self.reward_function()
 
         return self.reward, observation, False, self.get_info()
@@ -118,14 +101,8 @@ class PlantGrowthChamber(BaseAsyncEnvironment):
         old = self.normalize(iqm(self.observed_areas[-2], self.q))
         return new - old
 
-    def normalize(self, x, l=0, u=930):  # normalize area (mm^2) to between 0 and 1
+    def normalize(self, x, l=0.0, u=930.0):  # normalize area (mm^2) to between 0 and 1
         return (x - l) / (u - l)
-    
-    #def transform_time_sine(self, time, total=86400.0):
-    #    return np.array([np.sin(2 * np.pi * time / total), np.cos(2 * np.pi * time / total)])
-
-    def transform_time_linear(self, time, total=86400.0):
-        return time / total
 
     def close(self):
         requests.put(self.zone.lightbar_url, json={"array": np.zeros((2, 6)).tolist()})
