f8b9dc32d313bffc9edf09ffeb433a2d33ee20a1
Oliver Diamond <odiamond@wesleyan.edu>
2025-03-27 17:02:28 -0600

    tested trivial reward implementation

--- scripts/Linear.sh
+++ scripts/Linear.sh
@@ -3,4 +3,6 @@
 # exit script on error
 set -e
 
-python3 scripts/local.py --runs 5 -e experiments/offline/O1/E7-DQN-2week-sweeps/PlantSimulator/DQN.json
+python3 scripts/local.py --runs 5 -e experiments/offline/O1/E8-trivial_rew_1ep/DQN.json
+python3 scripts/local.py --runs 5 -e experiments/offline/O1/E8-trivial_rew_1ep/GAC.json
+
