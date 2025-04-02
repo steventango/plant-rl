5ae02223e32d2f890605e58789fc00bd31dd91b1
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-02 16:44:49 -0600

    Fix index

--- scripts/online_E5_P1.sh
+++ scripts/online_E5_P1.sh
@@ -3,5 +3,5 @@
 # exit script on error
 set -e
 
-python src/main_real.py -e experiments/online/E5/P1/Poisson.json -i 1
-python src/main_real.py -e experiments/online/E5/P1/Poisson-slow.json -i 1
+python src/main_real.py -e experiments/online/E5/P1/Poisson.json -i 0
+python src/main_real.py -e experiments/online/E5/P1/Poisson-slow.json -i 0
