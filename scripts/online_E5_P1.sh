bc5d002f8bdf1c3a2c7ce22e229d995d982ab78e
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-02 16:59:48 -0600

    Add tc-ESARSA config

--- scripts/online_E5_P1.sh
+++ scripts/online_E5_P1.sh
@@ -5,3 +5,4 @@ set -e
 
 python src/main_real.py -e experiments/online/E5/P1/Poisson.json -i 0
 python src/main_real.py -e experiments/online/E5/P1/Poisson-slow.json -i 0
+python src/main_real.py -e experiments/online/E5/P1/tc-ESARSA.json -i 0
