385ff504f60e3aaa34e6970ea5922802895caca7
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-04 16:50:01 -0600

    Reset trace after last update

--- scripts/offline_E6.sh
+++ scripts/offline_E6.sh
@@ -4,7 +4,7 @@
 set -e
 
 python src/main_real.py -e experiments/offline/E6/P0/tc-ESARSA.json -i 0
-python scripts/local.py --runs 30 --cpus 20 -e experiments/offline/E6/P1/ESARSA.json
+python scripts/local.py --runs 100 --cpus 18 -e experiments/offline/E6/P1/ESARSA.json
 python src/main_real.py -e experiments/offline/E6/P2/tc-ESARSA.json -i 0
 python src/main_real.py -e experiments/offline/E6/P2/Poisson-slow.json -i 0
 python src/main_real.py -e experiments/offline/E6/P2/Poisson.json -i 0
