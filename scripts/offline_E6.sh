b00d958bf9d5a09519d26d41c943127fd201835c
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-04 00:05:02 -0600

    E6/P2

--- scripts/offline_E6.sh
+++ scripts/offline_E6.sh
@@ -4,3 +4,5 @@
 set -e
 
 python src/main_real.py -e experiments/offline/E6/P0/tc-ESARSA.json -i 0
+python scripts/local.py --runs 30 --cpus 20 -e experiments/offline/E6/P1/ESARSA.json
+python src/main_real.py -e experiments/offline/E6/P2/tc-ESARSA.json -i 0
