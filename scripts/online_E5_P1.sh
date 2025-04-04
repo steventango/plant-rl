38b5ba0c48aed9e1c2c0462a59c04422dbe2ab9b
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-04 00:26:44 -0600

    Increase epsilon

--- scripts/online_E5_P1.sh
+++ scripts/online_E5_P1.sh
@@ -5,4 +5,4 @@ set -e
 
 python src/main_real.py -e experiments/online/E5/P1/Poisson.json -i 0
 python src/main_real.py -e experiments/online/E5/P1/Poisson-slow.json -i 0
-python src/main_real.py -e experiments/online/E5/P3/tc-ESARSA.json -i 0
+python src/main_real.py -e experiments/online/E5/P4/tc-ESARSA.json -i 0
