f7b408b683387b68237e3447c73d8f3d3cddbafc
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-04 00:08:45 -0600

    MountainCar experiment

--- experiments/offline/E6/P1/learning_curves.py
+++ experiments/offline/E6/P1/learning_curves.py
@@ -1,20 +1,23 @@
 import os
 import sys
-sys.path.append(os.getcwd() + '/src')
 
-import numpy as np
+sys.path.append(os.getcwd() + "/src")
+
 import matplotlib.pyplot as plt
-from PyExpPlotting.matplot import save, setDefaultConference
-from PyExpUtils.results.Collection import ResultCollection
-
-from RlEvaluation.config import data_definition
-from RlEvaluation.interpolation import compute_step_return
-from RlEvaluation.temporal import TimeSummary, extract_multiple_learning_curves, curve_percentile_bootstrap_ci
-from RlEvaluation.statistics import Statistic
-from RlEvaluation.utils.pandas import split_over_column
-
+import numpy as np
 import RlEvaluation.hypers as Hypers
 import RlEvaluation.metrics as Metrics
+from PyExpPlotting.matplot import save, setDefaultConference
+from PyExpUtils.results.Collection import ResultCollection
+from RlEvaluation.config import data_definition
+from RlEvaluation.interpolation import compute_step_return
+from RlEvaluation.statistics import Statistic
+from RlEvaluation.temporal import (
+    TimeSummary,
+    curve_percentile_bootstrap_ci,
+    extract_multiple_learning_curves,
+)
+from RlEvaluation.utils.pandas import split_over_column
 
 # from analysis.confidence_intervals import bootstrapCI
 from experiment.ExperimentModel import ExperimentModel
@@ -22,18 +25,15 @@ from experiment.tools import parseCmdLineArgs
 
 # makes sure figures are right size for the paper/column widths
 # also sets fonts to be right size when saving
-setDefaultConference('jmlr')
+setDefaultConference("jmlr")
 
 COLORS = {
-    'SoftmaxAC': 'grey',
-    'EQRC': 'blue',
-    'ESARSA': 'red',
-    'DQN': 'black',
-    'PrioritizedDQN': 'purple',
+    "SoftmaxAC": "grey",
+    "EQRC": "blue",
+    "ESARSA": "red",
+    "DQN": "black",
+    "PrioritizedDQN": "purple",
 }
-
-# keep 1 in every SUBSAMPLE measurements
-SUBSAMPLE = 100
 
 if __name__ == "__main__":
     path, should_save, save_type = parseCmdLineArgs()
@@ -42,11 +42,10 @@ if __name__ == "__main__":
 
     data_definition(
         hyper_cols=results.get_hyperparameter_columns(),
-        seed_col='seed',
-        time_col='frame',
-        environment_col='environment',
-        algorithm_col='algorithm',
-
+        seed_col="seed",
+        time_col="frame",
+        environment_col="environment",
+        algorithm_col="algorithm",
         # makes this data definition globally accessible
         # so we don't need to supply it to all API calls
         make_global=True,
@@ -56,65 +55,40 @@ if __name__ == "__main__":
         # converts path like "experiments/example/MountainCar"
         # into a new column "environment" with value "MountainCar"
         # None means to ignore a path part
-        folder_columns=(None, None, 'environment'),
-
+        folder_columns=(None, None, None, "environment"),
         # and creates a new column named "algorithm"
         # whose value is the name of an experiment file, minus extension.
         # For instance, ESARSA.json becomes ESARSA
-        file_col='algorithm',
+        file_col="algorithm",
     )
 
     assert df is not None
-    Metrics.add_step_weighted_return(df)
 
     exp = results.get_any_exp()
 
-    for env, env_df in split_over_column(df, col='environment'):
-        f, ax = plt.subplots()
-        for alg, sub_df in split_over_column(env_df, col='algorithm'):
-            if len(sub_df) == 0: continue
+    f, ax = plt.subplots()
+    for lambda_val, lambda_df in split_over_column(df, col="lambda"):
+        alphas = []
+        best_scores = []
+        for alpha_val, sub_df in sorted(split_over_column(lambda_df, col="alpha"), key=lambda x: x[0]):
+            sub_df = sub_df[sub_df["episode"] < 50]
+            print(sub_df["return"].count())
+            score = -np.nanmean(sub_df["return"])
+            print(lambda_val, alpha_val, f"{score:.2f}")
+            best_scores.append(score)
+            alphas.append(alpha_val)
 
-            report = Hypers.select_best_hypers(
-                sub_df,
-                metric='step_weighted_return',
-                prefer=Hypers.Preference.high,
-                time_summary=TimeSummary.sum,
-                statistic=Statistic.mean,
-            )
+        ax.plot(alphas, best_scores, label=lambda_val)
+    ax.set_xlim(0, 1.75)
+    ax.set_ylim(170, 400)
+    ax.set_title(
+        "Mountain Car\n"
+        r"ESARSA($\lambda$) with replacing traces"
+    )
+    ax.set_xlabel(r"$\alpha \times$ number of tilings (8)")
+    ax.set_ylabel("Mountain Car\nSteps per episode\n averaged over\nfirst 50 episodes\nand 30 runs", rotation=0)
+    ax.yaxis.set_label_coords(-0.2, 0.5)
 
-            print('-' * 25)
-            print(env, alg)
-            Hypers.pretty_print(report)
-
-            xs, ys = extract_multiple_learning_curves(
-                sub_df,
-                report.uncertainty_set_configurations,
-                metric='return',
-                interpolation=lambda x, y: compute_step_return(x, y, exp.total_steps),
-            )
-
-            xs = np.asarray(xs)[:, ::SUBSAMPLE]
-            ys = np.asarray(ys)[:, ::SUBSAMPLE]
-
-            # make sure all of the x values are the same for each curve
-            assert np.all(np.isclose(xs[0], xs))
-
-            res = curve_percentile_bootstrap_ci(
-                rng=np.random.default_rng(0),
-                y=ys,
-                statistic=Statistic.mean,
-            )
-
-            ax.plot(xs[0], res.sample_stat, label=alg, color=COLORS[alg], linewidth=0.5)
-            ax.fill_between(xs[0], res.ci[0], res.ci[1], color=COLORS[alg], alpha=0.2)
-
-        ax.legend()
-        if should_save:
-            save(
-                save_path=f'{path}/plots',
-                plot_name=f'{env}'
-            )
-            plt.clf()
-        else:
-            plt.show()
-            exit()
+    ax.legend()
+    save(save_path=f"{path}/plots", plot_name=f"ESARSA(lambda) with replacing traces")
+    plt.clf()
