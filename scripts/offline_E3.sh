954e5fe859a4e571840f0617c48f61b387e265cc
Anffany Chen <anffany@ualberta.ca>
2025-04-07 14:07:30 -0600

    tried fixed agent in sim

--- scripts/offline_E3.sh
+++ scripts/offline_E3.sh
@@ -3,4 +3,4 @@
 # exit script on error
 set -e
 
-python scripts/local.py --runs 5 --cpus 1 -e experiments/offline/E3/P15/tc-ESARSA-s3.json
+python scripts/local.py --runs 5 --cpus 1 -e experiments/offline/E3/P16/tc-ESARSA.json
