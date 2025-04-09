f03147aef2d31d9ccb93dc530b56f232c3c872e7
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-09 13:24:58 -0600

    Script to start E6/P2 experiment

--- scripts/online_E6.sh
+++ scripts/online_E6.sh
@@ -3,6 +3,11 @@
 # exit script on error
 set -e
 
-python src/main_real.py -e experiments/online/E6/P0/Spreadsheet.json -i 0
-python src/main_real.py -e experiments/online/E6/P0/Spreadsheet.json -i 1
-python src/main_real.py -e experiments/online/E6/P0/Spreadsheet.json -i 2
+current_epoch=$(date +%s)
+target_epoch=$(date -d '04/09/2025 13:30' +%s)
+sleep_seconds=$(( $target_epoch - $current_epoch ))
+sleep $sleep_seconds
+python src/main_real.py -e experiments/online/E6/P2/Spreadsheet1.json
+python src/main_real.py -e experiments/online/E6/P2/Spreadsheet2.json
+python src/main_real.py -e experiments/online/E6/P2/Spreadsheet6.json
+python src/main_real.py -e experiments/online/E6/P2/Spreadsheet9.json
