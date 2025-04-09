58553b59ed67b082ef4c63beb9840fbbeba32d65
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-09 13:32:07 -0600

    Edit experiment, test for 1h

--- scripts/online_E6.sh
+++ scripts/online_E6.sh
@@ -4,10 +4,10 @@
 set -e
 
 current_epoch=$(date +%s)
-target_epoch=$(date -d '04/09/2025 13:30' +%s)
+target_epoch=$(date -d '04/09/2025 13:40' +%s)
 sleep_seconds=$(( $target_epoch - $current_epoch ))
 sleep $sleep_seconds
-python src/main_real.py -e experiments/online/E6/P2/Spreadsheet1.json
-python src/main_real.py -e experiments/online/E6/P2/Spreadsheet2.json
-python src/main_real.py -e experiments/online/E6/P2/Spreadsheet6.json
-python src/main_real.py -e experiments/online/E6/P2/Spreadsheet9.json
+python src/main_real.py -e experiments/online/E6/P2/Spreadsheet1.json -i 0
+python src/main_real.py -e experiments/online/E6/P2/Spreadsheet2.json -i 0
+python src/main_real.py -e experiments/online/E6/P2/Spreadsheet6.json -i 0
+python src/main_real.py -e experiments/online/E6/P2/Spreadsheet9.json -i 0
