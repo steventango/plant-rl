2303fe512ac52a5c2f13c28328a88bd25a863124
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-09 13:34:30 -0600

    Start experiment at 13:40

--- scripts/online_E6.sh
+++ scripts/online_E6.sh
@@ -6,8 +6,27 @@ set -e
 current_epoch=$(date +%s)
 target_epoch=$(date -d '04/09/2025 13:40' +%s)
 sleep_seconds=$(( $target_epoch - $current_epoch ))
+echo $sleep_seconds
 sleep $sleep_seconds
 python src/main_real.py -e experiments/online/E6/P2/Spreadsheet1.json -i 0
+
+current_epoch=$(date +%s)
+target_epoch=$(date -d '04/09/2025 13:40' +%s)
+sleep_seconds=$(( $target_epoch - $current_epoch ))
+echo $sleep_seconds
+sleep $sleep_seconds
 python src/main_real.py -e experiments/online/E6/P2/Spreadsheet2.json -i 0
+
+current_epoch=$(date +%s)
+target_epoch=$(date -d '04/09/2025 13:40' +%s)
+sleep_seconds=$(( $target_epoch - $current_epoch ))
+echo $sleep_seconds
+sleep $sleep_seconds
 python src/main_real.py -e experiments/online/E6/P2/Spreadsheet6.json -i 0
+
+current_epoch=$(date +%s)
+target_epoch=$(date -d '04/09/2025 13:40' +%s)
+sleep_seconds=$(( $target_epoch - $current_epoch ))
+echo $sleep_seconds
+sleep $sleep_seconds
 python src/main_real.py -e experiments/online/E6/P2/Spreadsheet9.json -i 0
