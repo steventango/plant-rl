2a84d81f08606b9364e32f062bc46fc37d1bb548
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-09 16:53:24 -0600

    Fix spreadsheet

--- scripts/online_E6.sh
+++ scripts/online_E6.sh
@@ -4,29 +4,29 @@
 set -e
 
 current_epoch=$(date +%s)
-target_epoch=$(date -d '04/09/2025 13:40' +%s)
+target_epoch=$(date -d '04/09/2025 17:00' +%s)
 sleep_seconds=$(( $target_epoch - $current_epoch ))
 echo $sleep_seconds
 sleep $sleep_seconds
-python src/main_real.py -e experiments/online/E6/P2/Spreadsheet1.json -i 0
+python src/main_real.py -e experiments/online/E6/P3/Spreadsheet1.json -i 0
 
 current_epoch=$(date +%s)
-target_epoch=$(date -d '04/09/2025 13:40' +%s)
+target_epoch=$(date -d '04/09/2025 17:00' +%s)
 sleep_seconds=$(( $target_epoch - $current_epoch ))
 echo $sleep_seconds
 sleep $sleep_seconds
-python src/main_real.py -e experiments/online/E6/P2/Spreadsheet2.json -i 0
+python src/main_real.py -e experiments/online/E6/P3/Spreadsheet2.json -i 0
 
 current_epoch=$(date +%s)
-target_epoch=$(date -d '04/09/2025 13:40' +%s)
+target_epoch=$(date -d '04/09/2025 17:00' +%s)
 sleep_seconds=$(( $target_epoch - $current_epoch ))
 echo $sleep_seconds
 sleep $sleep_seconds
-python src/main_real.py -e experiments/online/E6/P2/Spreadsheet6.json -i 0
+python src/main_real.py -e experiments/online/E6/P3/Spreadsheet6.json -i 0
 
 current_epoch=$(date +%s)
-target_epoch=$(date -d '04/09/2025 13:40' +%s)
+target_epoch=$(date -d '04/09/2025 17:00' +%s)
 sleep_seconds=$(( $target_epoch - $current_epoch ))
 echo $sleep_seconds
 sleep $sleep_seconds
-python src/main_real.py -e experiments/online/E6/P2/Spreadsheet9.json -i 0
+python src/main_real.py -e experiments/online/E6/P3/Spreadsheet9.json -i 0
