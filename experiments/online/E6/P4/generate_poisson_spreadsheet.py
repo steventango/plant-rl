3a5a8a6bac70ec8895403c1a1b17a49eed30b948
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-09 23:38:29 -0600

    Resume poisson policies

---
+++ experiments/online/E6/P4/generate_poisson_spreadsheet.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+
+def generate_policy(filename, interval = 5, lam = 3):
+    days = 21
+    reference_action = np.array([0.398, 0.762, 0.324, 0.000, 0.332, 0.606])
+    data = []
+    total_minutes = 0
+    prev_action = np.zeros_like(reference_action)
+    day = 0
+    while total_minutes < days * 24 * 60:
+        delta = timedelta(minutes=total_minutes)
+        day = delta.days
+        if day >= days:
+            break
+        time_str = (datetime.min + delta).strftime("%H:%M:%S")
+        new_intensity = np.random.choice([0.350, 1, 0.675, 1.652])
+        new_action = reference_action * new_intensity
+        if len(data):
+            time_step_prev = (datetime.min + delta - timedelta(seconds=1)).strftime("%H:%M:%S")
+            data.append([day, time_step_prev, *prev_action, 1.0])
+        data.append([day, time_str, *new_action, 1.0])
+        prev_action = new_action
+        interval_step = np.random.poisson(lam) + 1
+        total_minutes += interval_step * interval
+    data.append([day, "23:59:59", *prev_action, 1.0])
+
+    df = pd.DataFrame(
+        data, columns=["Day", "Time", "Blue", "Cool_White", "Warm_White", "Orange_Red", "Red", "Far_Red", "Scaling"]
+    )
+    save_path = Path(__file__).parent / filename
+    df.to_excel(save_path, index=False)
+
+
+if __name__ == "__main__":
+    for zone in [1,2,6,9]:
+        generate_policy(f"poisson{zone}.xlsx", interval=5, lam=2)
--- scripts/online_E6.sh
+++ scripts/online_E6.sh
@@ -3,30 +3,7 @@
 # exit script on error
 set -e
 
-current_epoch=$(date +%s)
-target_epoch=$(date -d '04/09/2025 17:00' +%s)
-sleep_seconds=$(( $target_epoch - $current_epoch ))
-echo $sleep_seconds
-sleep $sleep_seconds
-python src/main_real.py -e experiments/online/E6/P3/Spreadsheet1.json -i 0
-
-current_epoch=$(date +%s)
-target_epoch=$(date -d '04/09/2025 17:00' +%s)
-sleep_seconds=$(( $target_epoch - $current_epoch ))
-echo $sleep_seconds
-sleep $sleep_seconds
-python src/main_real.py -e experiments/online/E6/P3/Spreadsheet2.json -i 0
-
-current_epoch=$(date +%s)
-target_epoch=$(date -d '04/09/2025 17:00' +%s)
-sleep_seconds=$(( $target_epoch - $current_epoch ))
-echo $sleep_seconds
-sleep $sleep_seconds
-python src/main_real.py -e experiments/online/E6/P3/Spreadsheet6.json -i 0
-
-current_epoch=$(date +%s)
-target_epoch=$(date -d '04/09/2025 17:00' +%s)
-sleep_seconds=$(( $target_epoch - $current_epoch ))
-echo $sleep_seconds
-sleep $sleep_seconds
-python src/main_real.py -e experiments/online/E6/P3/Spreadsheet9.json -i 0
+python src/main_real.py -e experiments/online/E6/P4/Poisson1.json -i 0
+python src/main_real.py -e experiments/online/E6/P4/Poisson2.json -i 0
+python src/main_real.py -e experiments/online/E6/P4/Poisson6.json -i 0
+python src/main_real.py -e experiments/online/E6/P4/Poisson9.json -i 0
