e8a9d816598fd391b6b2ae24b99bc9c2d6b8c238
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-24 15:01:23 -0600

    Add script to get images from all cameras

---
+++ scripts/all_cameras.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script to get images from all cameras
+
+# Array of zones to turn off
+hostnames=(zone01-camera01 zone01-camera02 zone02-camera01 zone02-camera02 zone03-camera01 zone03-camera02 zone06-camera01 zone08-camera01 zone09-camera01)
+
+for hostname in "${hostnames[@]}"; do
+  curl http://mitacs-${hostname}.ccis.ualberta.ca:8080/observation --output ${hostname}-observation.png
+done
