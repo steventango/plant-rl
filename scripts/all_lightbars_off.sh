a9960b1c2ea1431eed276648fa42dce36109de9f
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-24 15:01:34 -0600

    Add script to turn all lightbars off

---
+++ scripts/all_lightbars_off.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script to turn off lights in specified zones
+
+# Array of zones to turn off
+zones=(1 2 3 6 8 9)
+
+for zone in "${zones[@]}"; do
+  curl http://mitacs-zone$zone.ccis.ualberta.ca:8080/action -X PUT -H "Content-Type: application/json" -d '{"array": [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]}'
+done
