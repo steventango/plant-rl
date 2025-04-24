c9d56d75069418f4e5caf3cba0475e170e54b370
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-24 22:54:33 +0000

    Migrate data script for pre-plant-rl data

--- migrate_data.py
+++ migrate_data.py
@@ -5,14 +5,18 @@ from PIL import Image
 from tqdm.contrib.concurrent import thread_map
 
 
-def migrate(path: Path):
-    new_path = path.relative_to("data")
-    new_path = Path("/data") / new_path
-    new_path = new_path.parent / "images" / new_path.name
+def migrate(path: Path, dry_run: bool = False) -> None:
+    # new_path = path.relative_to("data")
+    # new_path = Path("/data") / path
+    new_path = path.parent / "images" / path.name
     new_path = new_path.with_suffix(".jpg")
 
     if not new_path.parent.exists():
         new_path.parent.mkdir(parents=True, exist_ok=True)
+
+    if dry_run:
+        print(f"Dry run: {path} would be migrated to {new_path}")
+        return
 
     img = Image.open(path)
     img = img.convert("RGB")
@@ -28,8 +32,9 @@ def migrate(path: Path):
 
 paths = sorted(
     chain(
-        Path("data/online/").glob("**/*.png"),
-        Path("data/online/").glob("**/*.jpg"),
+        Path("/data/maria_exp").glob("**/*.png"),
+        Path("/data/nazmus_exp").glob("**/*.png"),
+        Path("/data/phytochrome_exp").glob("**/*.png"),
     )
 )
 
