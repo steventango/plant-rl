01a369ee2d0cde58279730ac4a2c7ea11b8540da
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-25 04:12:53 +0000

    Updated script for making timelapses

--- scripts/make_timelapse.sh
+++ scripts/make_timelapse.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-ffmpeg -framerate 24 -pattern_type glob -i 'data/first_exp/z2cR/*.png' -c:v libx264 -pix_fmt yuv420p timelapse.mp4
+ffmpeg -framerate 24 -pattern_type glob -i '/data/plant-rl/online/E6/P5/Spreadsheet-Poisson6/z6/processed/v3.3.1/images/*_stacked.jpg' -c:v libx264 -pix_fmt yuv420p timelapse_6.mp4
+ffmpeg -framerate 24 -pattern_type glob -i '/data/plant-rl/online/E6/P5/Spreadsheet-Poisson9/z9/processed/v3.3.1/images/*_stacked.jpg' -c:v libx264 -pix_fmt yuv420p timelapse_9.mp4
