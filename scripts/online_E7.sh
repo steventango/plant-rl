e80be8823059023c39f4eaf676041cf35718c7d3
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-27 07:05:14 +0000

    Specify zone to save data in right place

--- scripts/online_E7.sh
+++ scripts/online_E7.sh
@@ -5,3 +5,4 @@ set -e
 
 python src/main_real.py -e experiments/online/E7/P0/Spreadsheet1.json -i 0
 python src/main_real.py -e experiments/online/E7/P0/Spreadsheet3.json -i 0
+python src/main_real.py -e experiments/online/E7/P1/ESARSA2.json -i 0
