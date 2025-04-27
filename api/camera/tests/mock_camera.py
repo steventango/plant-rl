04c5bfad577567e936347b6845570fba4b5e000c
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-27 09:57:56 -0600

    Swap png for jpg

--- api/camera/tests/mock_camera.py
+++ api/camera/tests/mock_camera.py
@@ -3,9 +3,9 @@ from PIL import Image
 
 
 class MockCamera:
-    def capture(self, stream, format='png'):
+    def capture(self, stream, format='jpeg'):
         np.random.seed(0)
         image = np.random.rand(1944, 2592, 3) * 255
         image = image.astype(np.uint8)
         image = Image.fromarray(image)
-        image.save(stream, format=format)
+        image.save(stream, format=format, quality=90)
