844df55248d6f265982a4287e872c76cce38ae4f
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-28 05:40:47 +0000

    Make work with nazmus_experiment

--- src/process_old_data.py
+++ src/process_old_data.py
@@ -15,9 +15,9 @@ def get_zone_from_config(config):
     config_zone = config["zone"]
     zone = Zone(
         identifier=config_zone["identifier"],
-        camera_left_url=config_zone["camera_left_url"],
-        camera_right_url=config_zone["camera_right_url"],
-        lightbar_url=config_zone["lightbar_url"],
+        camera_left_url=config_zone.get("camera_left_url"),
+        camera_right_url=config_zone.get("camera_right_url"),
+        lightbar_url=config_zone.get("lightbar_url"),
         trays=[
             Tray(
                 n_wide=tray["n_wide"],
@@ -36,19 +36,23 @@ def get_zone_from_config(config):
 
 
 def main():
-    datasets = Path("/data/plant-rl/online/E6/P5").glob("Spreadsheet-Poisson*/z*")
+    datasets = Path("/data/plant-rl").glob("nazmus_exp/z11c1")
     datasets = sorted(datasets)
-    datasets = datasets[2:]
-    pipeline_version = "v3.3.1"
+
+    pipeline_version = "v3.3.2"
     for dataset in datasets:
         with open(next(dataset.rglob("config.json"))) as f:
             config = json.load(f)
             zone = get_zone_from_config(config)
-        raw_df = pd.read_csv(dataset / "raw.csv")
+        try:
+            raw_df = pd.read_csv(dataset / "raw.csv")
+        except FileNotFoundError:
+            print(f"raw.csv not found in {dataset}.")
+            raw_df = pd.DataFrame()
         out_dir = dataset / "processed" / pipeline_version
         out_dir_images = out_dir / "images"
         out_dir_images.mkdir(exist_ok=True, parents=True)
-        paths = sorted((dataset / "images").glob("*.jpg"))[1:]
+        paths = sorted((dataset / "images").glob("*.jpg"))[::2]
 
         dfs = []
         for i, path in tqdm(enumerate(paths), total=len(paths)):
@@ -56,6 +60,13 @@ def main():
             dfs.append(df)
 
         new_df = pd.concat(dfs)
+
+        if raw_df.empty:
+            # if raw_df is empty, just save new_df
+            new_df["time"] = pd.to_datetime(new_df["image_name"].str.extract(r'--(\d{4}-\d{2}-\d{2})--(\d{2}-\d{2}-\d{2})')[0] + ' ' + new_df["image_name"].str.extract(r'--(\d{2}-\d{2}-\d{2})')[0], format='%Y-%m-%d %H-%M-%S')
+            new_df.to_csv(out_dir / "all.csv", index=False)
+            continue
+
         # raw_df drop cols that are in new_df
         cols_to_drop = [col for col in raw_df.columns if col in new_df.columns]
         cols_to_drop.remove("frame")
@@ -81,6 +92,7 @@ def process_one_image(zone, out_dir, path, index):
     debug_images = {}
     df = process_image(image, zone.trays, debug_images)
     df["frame"] = index
+    df["image_name"] = path.name
     for key, image in debug_images.items():
         image.save(out_dir / f"{isoformat}_{key}.jpg")
     return df
