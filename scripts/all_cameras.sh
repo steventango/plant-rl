d2f155d2bf495d4a556903bcb8bf7630c26a4399
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-29 01:35:59 +0000

    Update camera update commands

--- scripts/all_cameras.sh
+++ scripts/all_cameras.sh
@@ -5,5 +5,5 @@
 hostnames=(zone01-camera01 zone01-camera02 zone02-camera01 zone02-camera02 zone03-camera01 zone03-camera02 zone06-camera01 zone08-camera01 zone09-camera01)
 
 for hostname in "${hostnames[@]}"; do
-  curl http://mitacs-${hostname}.ccis.ualberta.ca:8080/observation --output ${hostname}-observation.png
+  curl http://mitacs-${hostname}.ccis.ualberta.ca:8080/observation --output ${hostname}-observation.jpg
 done
