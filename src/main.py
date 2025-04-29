744a522b626b1a74a7ac8e87309503804ea72cde
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-04-29 21:36:14 +0000

    Add wandb logging to main

--- src/main.py
+++ src/main.py
@@ -1,26 +1,32 @@
-import Box2D     # we need to import this first because cedar is stupid
 import os
 import sys
-sys.path.append(os.getcwd())
-from tqdm import tqdm
 
-import time
+import Box2D  # we need to import this first because cedar is stupid
+
+sys.path.append(os.getcwd())
+import argparse
+import logging
 import random
 import socket
-import logging
-import argparse
+import time
+from pathlib import Path
+
 import numpy as np
 import torch
-from RlGlue import RlGlue
-from experiment import ExperimentModel
-from utils.checkpoint import Checkpoint
-from utils.preempt import TimeoutHandler
-from problems.registry import getProblem
-from PyExpUtils.results.sqlite import saveCollector
 from PyExpUtils.collection.Collector import Collector
-from PyExpUtils.collection.Sampler import Ignore, MovingAverage, Subsample, Identity
-from utils.window_avg import WindowAverage
+from PyExpUtils.collection.Sampler import Identity, Ignore, MovingAverage, Subsample
 from PyExpUtils.collection.utils import Pipe
+from PyExpUtils.results.sqlite import saveCollector
+from tqdm import tqdm
+
+import wandb
+from experiment import ExperimentModel
+from problems.registry import getProblem
+from utils.checkpoint import Checkpoint
+from utils.logger import log
+from utils.preempt import TimeoutHandler
+from utils.window_avg import WindowAverage
+from utils.RlGlue.rl_glue import LoggingRlGlue
 
 # ------------------
 # -- Command Args --
@@ -39,6 +45,7 @@ args = parser.parse_args()
 # -- Library Configuration --
 # ---------------------------
 import jax
+
 device = 'gpu' if args.gpu else 'cpu'
 jax.config.update('jax_platform_name', device)
 
@@ -102,20 +109,40 @@ for idx in indices:
         problem.params['total_steps'] = env.terminal_step
         exp.total_steps = env.terminal_step
 
-    glue = chk.build('glue', lambda: RlGlue(agent, env))
+    glue = chk.build('glue', lambda: LoggingRlGlue(agent, env))
     chk.initial_value('episode', 0)
+
+    context = exp.buildSaveContext(idx, base=args.save_path)
+    agent_path = Path(context.resolve()).relative_to('results')
+
+    config = {
+        **problem.params,
+        "context": str(agent_path)
+    }
+
+    wandb_run = wandb.init(
+        entity="plant-rl",
+        project="main",
+        notes=str(agent_path),
+        config=config,
+        settings=wandb.Settings(
+            x_stats_disk_paths=("/", "/data"),
+        ),
+    )
 
     # Run the experiment
     start_time = time.time()
 
     # if we haven't started yet, then make the first interaction
     if glue.total_steps == 0:
-        glue.start()
+        s, a, info = glue.start()
+        log(env, glue, wandb_run, s, a, info)
 
     for step in range(glue.total_steps, exp.total_steps):
         collector.next_frame()
         chk.maybe_save()
         interaction = glue.step()
+        log(env, glue, wandb_run, interaction.o, interaction.a, interaction.extra, interaction.r)
 
         collector.collect('reward', interaction.r)
         collector.collect('episode', chk['episode'])
