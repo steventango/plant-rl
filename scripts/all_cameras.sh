1b6c0255b08f79fbd80dcb6783b83dc6cbcaf6cf
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-05-01 22:20:01 +0000

    Take pictures in parallel switch to camera02

--- scripts/all_cameras.sh
+++ scripts/all_cameras.sh
@@ -2,8 +2,8 @@
 # Script to get images from all cameras
 
 # Array of zones to turn off
-hostnames=(zone01-camera01 zone01-camera02 zone02-camera01 zone02-camera02 zone03-camera01 zone03-camera02 zone06-camera01 zone08-camera01 zone09-camera01)
+hostnames=(zone01-camera01 zone01-camera02 zone02-camera01 zone02-camera02 zone03-camera01 zone03-camera02 zone06-camera02 zone08-camera02 zone09-camera02)
 
 for hostname in "${hostnames[@]}"; do
-  curl http://mitacs-${hostname}.ccis.ualberta.ca:8080/observation --output ${hostname}-observation.jpg
+  curl http://mitacs-${hostname}.ccis.ualberta.ca:8080/observation --output ${hostname}-observation.jpg &
 done
