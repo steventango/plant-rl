1ee383695568cd3dacf5bd017bc50df78ca8e04a
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-05-04 14:12:06 +0000

    Mock experiment configs

--- scripts/offline_S2.sh
+++ scripts/offline_S2.sh
@@ -3,4 +3,10 @@
 # exit script on error
 set -e
 
-python src/main_real.py -e experiments/offline/S2/P1/ESARSA.json -i 0
+python src/main_real.py -e experiments/offline/S2/P1/Bandit.json -i 0 &
+python src/main_real.py -e experiments/offline/S2/P1/ContextualBandit.json -i 0 &
+python src/main_real.py -e experiments/offline/S2/P1/Constant-DIM.json -i 0 &
+python src/main_real.py -e experiments/offline/S2/P1/Constant-ON.json -i 0 &
+python src/main_real.py -e experiments/offline/S2/P1/ESARSA-A.json -i 0 &
+python src/main_real.py -e experiments/offline/S2/P1/ESARSA-B.json -i 0 &
+wait
