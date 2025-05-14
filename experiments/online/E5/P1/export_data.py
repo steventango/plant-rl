2ed79f20ffe965555cc3b5833c71d6b143982507
Oliver Diamond <87337110+oliverdiamond@users.noreply.github.com>
2025-05-14 22:28:05 +0000

    updated export_data.py for online/E5

--- experiments/online/E5/P1/export_data.py
+++ experiments/online/E5/P1/export_data.py
@@ -3,18 +3,21 @@ import sys
 
 sys.path.append(os.getcwd() + "/src")
 import numpy as np
-from PyExpPlotting.matplot import setDefaultConference
+import pandas as pd
 from PyExpUtils.results.Collection import ResultCollection
 from RlEvaluation.config import data_definition
+from RlEvaluation.utils.pandas import split_over_column
 
 from experiment.ExperimentModel import ExperimentModel
 from experiment.tools import parseCmdLineArgs
 
-setDefaultConference("neurips")
 
-COLORS = {"tc-ESARSA": "blue"}
-
-total_days = 14
+def maybe_convert_to_array(x):
+    if isinstance(x, float) or isinstance(x, int):
+        return x
+    if isinstance(x, bytes):
+        return np.frombuffer(x)
+    return x
 
 
 def main():
@@ -22,7 +25,7 @@ def main():
 
     results = ResultCollection.fromExperiments(Model=ExperimentModel)
 
-    data_definition(
+    dd = data_definition(
         hyper_cols=results.get_hyperparameter_columns(),
         seed_col="seed",
         time_col="frame",
@@ -38,7 +41,23 @@ def main():
 
     assert df is not None
 
+    for metric in ["action"]:
+        df[metric] = df[metric].apply(maybe_convert_to_array)
+
+    dfs = []
+    for alg, sub_df in split_over_column(df, col="algorithm"):
+
+        x = sub_df[dd.time_col].to_numpy()
+
+        idx = np.argwhere(x[1:] <= x[:-1])
+
+        if idx.size > 0:
+            sub_df = sub_df.iloc[idx[-1][0] + 1 :]
+        dfs.append(sub_df)
+
+    df = pd.concat(dfs, ignore_index=True)
     df.to_csv(f"{path}/data.csv", index=False)
+
 
 if __name__ == "__main__":
     main()
