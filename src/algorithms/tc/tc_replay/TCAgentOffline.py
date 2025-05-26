0395cd0eccd5186c00bf6a03aa785153ec5d5609
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-05-26 18:04:31 +0000

    Add TCAgentOffline

---
+++ src/algorithms/tc/tc_replay/TCAgentOffline.py
@@ -0,0 +1,93 @@
+from abc import abstractmethod
+from typing import Any, Dict, Tuple
+
+import numpy as np
+from PyExpUtils.collection.Collector import Collector
+from ReplayTables.interface import Timestep
+from ReplayTables.registry import build_buffer
+
+from algorithms.tc.TCAgent import TCAgent
+from utils.checkpoint import checkpointable
+
+
+@checkpointable(("buffer", "steps", "updates"))
+class TCAgentReplay(TCAgent):
+    def __init__(self, observations: Tuple[int, ...], actions: int, params: Dict, collector: Collector, seed: int):
+        super().__init__(observations, actions, params, collector, seed)
+
+        # Params for replay
+        self.buffer_size = params["buffer_size"]
+        self.batch_size = params["batch"]
+        self.update_freq = params.get("update_freq", 1)
+        self.updates_per_step = params.get("updates_per_step", 1)
+
+        self.buffer = build_buffer(
+            buffer_type=params["buffer_type"],
+            max_size=self.buffer_size,
+            lag=self.n_step,
+            rng=self.rng,
+            config=params.get("buffer_config", {}),
+        )
+
+        self.steps = 0
+        self.updates = 0
+
+    @abstractmethod
+    def batch_update(self): ...
+
+    # ----------------------
+    # -- RLGlue interface --
+    # ----------------------
+    def load_start(self, s: np.ndarray, a: int):
+        self.buffer.flush()
+
+        x = self.get_rep(s)
+        self.buffer.add_step(
+            Timestep(
+                x=x,
+                a=a,
+                r=None,
+                gamma=self.gamma,
+                terminal=False,
+            )
+        )
+        return {}
+
+    def load_step(self, r: float, sp: np.ndarray | None, a: int, extra: Dict[str, Any]):
+        a = -1
+
+        # sample next action
+        xp = None
+        if sp is not None:
+            xp = self.get_rep(sp)
+
+        # see if the problem specified a discount term
+        gamma = extra.get("gamma", 1.0)
+
+        self.buffer.add_step(
+            Timestep(
+                x=xp,
+                a=a,
+                r=r,
+                gamma=self.gamma * gamma,
+                terminal=False,
+            )
+        )
+
+        return {}
+
+    def load_end(self, r: float, extra: Dict[str, Any]):
+        self.buffer.add_step(
+            Timestep(
+                x=np.zeros(self.n_features),
+                a=-1,
+                r=r,
+                gamma=0,
+                terminal=True,
+            )
+        )
+
+        return {}
+
+    def plan(self):
+        self.batch_update()
