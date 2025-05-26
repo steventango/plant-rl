5abb5af73b30bfd7791bbda731950034fd586755
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-05-26 18:12:41 +0000

    OfflinePlantGrowthChamber problem

--- src/problems/OfflinePlantGrowthChamber.py
+++ src/problems/OfflinePlantGrowthChamber.py
@@ -2,13 +2,13 @@ from PyExpUtils.collection.Collector import Collector
 
 from environments.PlantGrowthChamber.OfflinePlantGrowthChamber import OfflinePlantGrowthChamber as Env
 from experiment.ExperimentModel import ExperimentModel
-from problems.BaseAsyncProblem import BaseAsyncProblem
+from problems.BaseProblem import BaseProblem
 
 
-class OfflinePlantGrowthChamber(BaseAsyncProblem):
+class OfflinePlantGrowthChamber(BaseProblem):
     def __init__(self, exp: ExperimentModel, idx: int, collector: Collector):
         super().__init__(exp, idx, collector)
 
         self.env = Env(**self.env_params)
-        self.actions = 6
-        self.observations = (1,)
+        self.actions = 2
+        self.observations = (2,)
