4479dacd3b31d5788b9633f5255185fb8cc63a29
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-06-03 14:47:36 +0000

    agent configs for zone 1 and 6

--- experiments/online/E8/P1/generate_bernoulli_spreadsheet.py
+++ experiments/online/E8/P1/generate_bernoulli_spreadsheet.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 
-def generate_policy(filename, interval = 10, lam = 3):
+def generate_policy(filename, interval = 10):
     days = 28
     reference_action = np.array([0.398, 0.762, 0.324, 0.000, 0.332, 0.606])
     data = []
@@ -15,6 +15,8 @@ def generate_policy(filename, interval = 10, lam = 3):
 
     # For each day, add structured entries for day/night transitions
     for day in range(days):
+        # linearly interpolate p from 0.5 to 1.0
+        p = 0.5 + (day / days) * 0.5
         prev_action = np.zeros_like(reference_action)
 
         # Generate poisson-based entries during daytime (9:00-21:00)
@@ -29,11 +31,8 @@ def generate_policy(filename, interval = 10, lam = 3):
             time_step_prev = (datetime.min + delta - timedelta(seconds=1)).strftime("%H:%M:%S")
             data.append([day, time_step_prev, *prev_action, 1.0])
 
-            # Flip intensity
-            if intensity == 1:
-                intensity = 0.675
-            else:
-                intensity = 1
+            # choose intensity based on p
+            intensity = np.random.choice([0.675, 1], p=[1 - p, p])
 
             # Apply new action
             new_action = reference_action * intensity
@@ -41,8 +40,7 @@ def generate_policy(filename, interval = 10, lam = 3):
             prev_action = new_action
 
             # Calculate next interval
-            poisson_sample = min(np.random.poisson(lam), 5)
-            interval_minutes = interval * (poisson_sample + 1)
+            interval_minutes = interval
             current_minutes += interval_minutes
 
             # Don't exceed 20:30
@@ -64,5 +62,4 @@ def generate_policy(filename, interval = 10, lam = 3):
 
 
 if __name__ == "__main__":
-    for zone in [1]:
-        generate_policy(f"poisson{zone}.xlsx", interval=10, lam=3)
+    generate_policy(f"bernoulli6.xlsx")
