be123caed149cf870e09e0a73404af67e35a0223
Anffany Chen <anffany@ualberta.ca>
2025-06-04 14:59:56 -0600

    consolidate and unify CleanPlantSim and OfflineEnv

--- src/environments/CleanPlantSimulator.py
+++ src/environments/CleanPlantSimulator.py
@@ -9,17 +9,20 @@ class CleanPlantSimulator(BaseEnvironment):
     '''
     Simulate the evolution of plant area based on lighting conditions.
     Assumptions: (i) Plant motion throughout each day is the same gaussian curve.
-                 (ii) Growth only occurs at night. Overnight growth is 30% if lighting is standard.
+                 (ii) Growth only occurs at night. Overnight growth is 20% if lighting is standard.
                  (iii) Changing lighting only affects overnight growth.
-    State = [time of day, area, daily light integral (dli)] (all normalized to [0,1])
+    State = [time of day, 
+             daily light integral (dli),
+             plant area, 
+             plant openness] (all normalized to [0,1])
     Action = [low, standard]
-    Reward = overnight growth assigned to last time stamp of each day 
+    Reward = percentage or raw overnight growth assigned to last time stamp of each day 
     Time Step = 10 min
     Episode duration = 14 days
     '''
     def __init__(self, **kwargs):
-        self.state_dim = (3,)
-        self.current_state = np.empty(3)
+        self.state_dim = (4,)
+        self.current_state = np.empty(4)
         self.action_dim = 2
         self.actions = [0, 1]
 
@@ -32,15 +35,33 @@ class CleanPlantSimulator(BaseEnvironment):
 
         self.gamma = 1.0
 
+        self.daily_reward = kwargs.get("daily_reward", True) 
+
     def get_observation(self):
         area = self.current_morning_size + self.daily_area_curve(self.steps % self.steps_per_day)
+
         tod = self.steps % self.steps_per_day
         daily_light_integral = self.dli
+        openness = area / self.current_morning_size
 
         observation = np.hstack([self.normalize(tod, 0, self.steps_per_day),
+                                 self.normalize(daily_light_integral, 0, self.steps_per_day),
                                  self.normalize(area, 1, 14),
-                                 self.normalize(daily_light_integral, 0, self.steps_per_day)])
+                                 self.normalize(openness, 0.97, 1.15)
+                                 ])
         return observation
+
+    def get_reward(self, percent_overnight_growth):
+        if self.daily_reward: 
+            return self.normalize(percent_overnight_growth, 0, 0.2)
+        else: 
+            return self.normalize(self.current_morning_size * percent_overnight_growth, 0, 2)
+    
+    def get_light_amount(self, action):
+        if action == 1:
+            return 1.0
+        else:   
+            return 0.5
     
     def start(self):
         self.steps = 0
@@ -53,12 +74,12 @@ class CleanPlantSimulator(BaseEnvironment):
 
     def step(self, action):
         self.steps += 1
-        self.dli += action
+        self.dli += self.get_light_amount(action)
         
         if self.steps % self.steps_per_day == 0:    # if transitioning overnight
-            overnight_growth = self.current_morning_size * 0.2 * self.normalize(self.dli, 0, self.steps_per_day)
-            self.current_morning_size += overnight_growth
-            self.reward = overnight_growth
+            percent_overnight_growth = 0.2 * self.normalize(self.dli, 0, self.steps_per_day)
+            self.reward = self.get_reward(percent_overnight_growth)
+            self.current_morning_size += self.current_morning_size * percent_overnight_growth
             self.dli = 0
         else: 
             self.reward = 0
@@ -86,41 +107,3 @@ class CleanPlantSimulator(BaseEnvironment):
         gaussian = self.current_morning_size * np.exp(-0.5 * ((x - mu) / stdev) ** 2)
         shift = self.current_morning_size * np.exp(-0.5 * ((0 - mu) / stdev) ** 2)
         return gaussian - shift
-    
-
-class CleanPlantSimulator_Daily(CleanPlantSimulator):
-    '''
-    Scale down the observed area and the overnight reward so that they are consistent throughout 14 days
-    State = [time of day, daily light integral (dli), daily area] (all normalized to [0,1])
-    Reward = percentage overnight growth assigned to last time stamp of each day 
-    '''
-    def get_observation(self):
-        area = self.current_morning_size + self.daily_area_curve(self.steps % self.steps_per_day)
-
-        tod = self.steps % self.steps_per_day
-        daily_area = area / self.current_morning_size   # daily area is area scaled by morning area
-        daily_light_integral = self.dli
-
-        observation = np.hstack([self.normalize(tod, 0, self.steps_per_day),
-                                 self.normalize(daily_area, 1, 1.12),
-                                 self.normalize(daily_light_integral, 0, self.steps_per_day)])
-        return observation
-
-    def step(self, action):
-        self.steps += 1
-        self.dli += action
-        
-        if self.steps % self.steps_per_day == 0:    # if transitioning overnight
-            percent_overnight_growth = 0.2 * self.normalize(self.dli, 0, self.steps_per_day)
-            self.current_morning_size += self.current_morning_size * percent_overnight_growth
-            self.reward = percent_overnight_growth
-            self.dli = 0
-        else: 
-            self.reward = 0
- 
-        self.current_state = self.get_observation()
-
-        if self.steps == self.total_steps:
-            return self.reward, self.current_state, True, self.get_info()
-        else:
-            return self.reward, self.current_state, False, self.get_info()
--- src/problems/CleanPlantSimulator.py
+++ src/problems/CleanPlantSimulator.py
@@ -1,5 +1,5 @@
 from PyExpUtils.collection.Collector import Collector
-from environments.CleanPlantSimulator import CleanPlantSimulator, CleanPlantSimulator_Daily
+from environments.CleanPlantSimulator import CleanPlantSimulator
 from experiment.ExperimentModel import ExperimentModel
 from problems.BaseProblem import BaseProblem
 
@@ -7,11 +7,6 @@ class SimplePlantSimulator(BaseProblem):
     def __init__(self, exp: ExperimentModel, idx: int, collector: Collector):
         super().__init__(exp, idx, collector)
         self.actions = 2
-        self.observations = (3,)
+        self.observations = (4,)
         self.gamma = 1.0
-        if self.env_params['type'] == 'default':
-            self.env = CleanPlantSimulator(**self.env_params)
-        elif self.env_params['type'] == 'daily':
-            self.env = CleanPlantSimulator_Daily(**self.env_params)
-        else:
-            raise ValueError(f"Invalid argument. Expected one of {{'default', 'daily'}}, but got: {self.env_params['type']}")
+        self.env = CleanPlantSimulator(**self.env_params)
