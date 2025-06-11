b355ea7978d4b2eb5a1c8b5d7826c6eb235dc052
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-06-11 18:55:33 +0000

    Auto format

--- src/problems/OfflinePlantGrowthChamber.py
+++ src/problems/OfflinePlantGrowthChamber.py
@@ -1,7 +1,12 @@
 from PyExpUtils.collection.Collector import Collector
 
 from environments.PlantGrowthChamber.OfflinePlantGrowthChamber import OfflinePlantGrowthChamber as Env
-from environments.PlantGrowthChamber.OfflinePlantGrowthChamber import OfflinePlantGrowthChamber_1hrStep, OfflinePlantGrowthChamber_1hrStep_MC, OfflinePlantGrowthChamber_1hrStep_MC_AreaOnly, OfflinePlantGrowthChamber_1hrStep_MC_TimeOnly
+from environments.PlantGrowthChamber.OfflinePlantGrowthChamber import (
+    OfflinePlantGrowthChamber_1hrStep,
+    OfflinePlantGrowthChamber_1hrStep_MC,
+    OfflinePlantGrowthChamber_1hrStep_MC_AreaOnly,
+    OfflinePlantGrowthChamber_1hrStep_MC_TimeOnly,
+)
 from experiment.ExperimentModel import ExperimentModel
 from problems.BaseProblem import BaseProblem
 
@@ -11,19 +16,19 @@ class OfflinePlantGrowthChamber(BaseProblem):
         super().__init__(exp, idx, collector)
         self.actions = 2
 
-        if self.env_params['type'] == 'default':
+        if self.env_params["type"] == "default":
             self.env = Env(**self.env_params)
             self.observations = (4,)
-        elif self.env_params['type'] == '1hrStep':
+        elif self.env_params["type"] == "1hrStep":
             self.env = OfflinePlantGrowthChamber_1hrStep(**self.env_params)
             self.observations = (4,)
-        elif self.env_params['type'] == '1hrStep_MC':
+        elif self.env_params["type"] == "1hrStep_MC":
             self.env = OfflinePlantGrowthChamber_1hrStep_MC(**self.env_params)
             self.observations = (4,)
-        elif self.env_params['type'] == '1hrStep_MC_AreaOnly':
+        elif self.env_params["type"] == "1hrStep_MC_AreaOnly":
             self.env = OfflinePlantGrowthChamber_1hrStep_MC_AreaOnly(**self.env_params)
             self.observations = (2,)
-        elif self.env_params['type'] == '1hrStep_MC_TimeOnly':
+        elif self.env_params["type"] == "1hrStep_MC_TimeOnly":
             self.env = OfflinePlantGrowthChamber_1hrStep_MC_TimeOnly(**self.env_params)
             self.observations = (2,)
         else:
