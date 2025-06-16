a3ea482edd4b2b6aed652ea10adc62b37d688a24
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-06-16 21:15:22 +0000

    Test different datasets

--- scripts/offline_S6.sh
+++ scripts/offline_S6.sh
@@ -5,3 +5,7 @@ set -e
 
 python src/main_offline.py -e experiments/offline/S6/P1/OfflineESARSA.json -i 0
 python src/main_offline.py -e experiments/offline/S6/P2/OfflineESARSA.json -i 0
+python src/main_offline.py -e experiments/offline/S6/P3/OfflineESARSA.json -i 0
+python src/main_offline.py -e experiments/offline/S6/P4/OfflineESARSA.json -i 0
+python src/main_offline.py -e experiments/offline/S6/P5/OfflineESARSA.json -i 0
+python src/main_offline.py -e experiments/offline/S6/P6/OfflineESARSA.json -i 0
