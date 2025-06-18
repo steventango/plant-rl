3c9c7ea837feed2405eb8781e71a0307a7037e64
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-06-18 19:57:38 +0000

    Test out more reward types

--- scripts/offline_S6.sh
+++ scripts/offline_S6.sh
@@ -3,8 +3,8 @@
 # exit script on error
 set -e
 
-python src/main_offline.py -e experiments/offline/S6/P8/OfflineESARSA.json -i 0 &
-python src/main_offline.py -e experiments/offline/S6/P9/OfflineESARSA.json -i 0 &
-python src/main_offline.py -e experiments/offline/S6/P10/OfflineESARSA.json -i 0 &
 python src/main_offline.py -e experiments/offline/S6/P11/OfflineESARSA.json -i 0 &
+python src/main_offline.py -e experiments/offline/S6/P12/OfflineESARSA.json -i 0 &
+python src/main_offline.py -e experiments/offline/S6/P13/OfflineESARSA.json -i 0 &
+python src/main_offline.py -e experiments/offline/S6/P14/OfflineESARSA.json -i 0 &
 wait
