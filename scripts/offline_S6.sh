0e8b3c4b93b385a4b5d10bfca22c8e89cda01480
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-06-18 19:34:19 +0000

    Add a variety of reward types to OfflinePlantGrowthChamber

--- scripts/offline_S6.sh
+++ scripts/offline_S6.sh
@@ -3,9 +3,8 @@
 # exit script on error
 set -e
 
-python src/main_offline.py -e experiments/offline/S6/P1/OfflineESARSA.json -i 0
-python src/main_offline.py -e experiments/offline/S6/P2/OfflineESARSA.json -i 0
-python src/main_offline.py -e experiments/offline/S6/P3/OfflineESARSA.json -i 0
-python src/main_offline.py -e experiments/offline/S6/P4/OfflineESARSA.json -i 0
-python src/main_offline.py -e experiments/offline/S6/P5/OfflineESARSA.json -i 0
-python src/main_offline.py -e experiments/offline/S6/P6/OfflineESARSA.json -i 0
+python src/main_offline.py -e experiments/offline/S6/P8/OfflineESARSA.json -i 0 &
+python src/main_offline.py -e experiments/offline/S6/P9/OfflineESARSA.json -i 0 &
+python src/main_offline.py -e experiments/offline/S6/P10/OfflineESARSA.json -i 0 &
+python src/main_offline.py -e experiments/offline/S6/P11/OfflineESARSA.json -i 0 &
+wait
