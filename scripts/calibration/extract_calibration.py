9cb0e9c6f90d12ace7b47fe8b9953677f4c28c5a
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-07-04 19:41:37 +0000

    Update calibration curves

--- scripts/calibration/extract_calibration.py
+++ scripts/calibration/extract_calibration.py
@@ -4,61 +4,9 @@
 import json
 import os
 
-import numpy as np
 import pandas as pd
 
-
-def get_far_red_integrals(zone: int, calibration_file_path: str) -> pd.Series:
-    """Reads the spectral data for a given zone, calculates the integral for each action,
-    and returns a Series with the integrals indexed by action."""
-    spectral_file = os.path.join(
-        os.path.dirname(calibration_file_path), f"RL_FarRedIntensityz{zone}.txt"
-    )
-    if not os.path.exists(spectral_file):
-        print(
-            f"Warning: Spectral file for zone {zone} not found. Falling back to zone 12."
-        )
-        spectral_file = os.path.join(
-            os.path.dirname(calibration_file_path), "RL_FarRedIntensityz12.txt"
-        )
-
-    df = pd.read_csv(spectral_file, sep="\t", header=0)
-
-    # Clean the DataFrame
-    df = df.iloc[:, :-1]  # Drop last column
-    df = df.drop(df.columns[1], axis=1)  # Drop second column
-    df = df.drop(df.columns[range(1, df.shape[1], 2)], axis=1)  # Drop odd columns
-
-    # Rename columns
-    action_values = [
-        0.10,
-        0.20,
-        0.25,
-        0.30,
-        0.35,
-        0.40,
-        0.50,
-        0.60,
-        0.70,
-        0.80,
-        0.85,
-        0.90,
-        0.95,
-        1.00,
-    ]
-    df.columns = ["Wavelength"] + action_values
-
-    # Drop 0.10
-    df = df.drop(columns=[0.10])
-    action_values.remove(0.10)
-
-    # Calculate integrals
-    integrals = {}
-    for action in action_values:
-        integrals[action] = np.trapz(df[action], df["Wavelength"])
-
-    return pd.Series(integrals)
-
+from utils.calibration import load_and_clean_data
 
 # Load the calibration data
 # one sheet per zone
@@ -66,6 +14,9 @@ def get_far_red_integrals(zone: int, calibration_file_path: str) -> pd.Series:
 calibration_file = (
     "/workspaces/plant-rl/scripts/calibration/Plant Chamber Full Calibration.xlsx"
 )
+
+maximum = {}
+safe_maximum = {}
 
 for zone in range(1, 13):
     zone_str = f"zone{zone:02}"
@@ -80,12 +31,54 @@ for zone in range(1, 13):
     df.reset_index(drop=True, inplace=True)
 
     # Get Far Red integrals
-    far_red_integrals = get_far_red_integrals(zone, calibration_file)
-    df["Far Red"] = df["Action"].map(far_red_integrals).fillna(0)  # type: ignore
+    _, integrals = load_and_clean_data(
+        os.path.join(
+            os.path.dirname(calibration_file),
+            f"RL_FarRedIntensityz{zone}.txt",
+        )
+    )
+    print(integrals)
+
+    df["far_red"] = df["Action"].map(integrals)  # type: ignore
+
+    # Rename columns to lower snake case
+    df.columns = [col.replace(" ", "_").lower() for col in df.columns]
 
     # Convert the DataFrame to a dictionary with lists as values
     df_dict = df.to_dict(orient="list")  # type: ignore
+    # if entire column is NaN, set it to None
+    df_dict = {k: None if all(pd.isna(v)) else v for k, v in df_dict.items()}
     print(df_dict)
+
+    actions = df_dict.keys()
+
+    actions_without_action = [action for action in actions if action not in ["action"]]
+
+    # Store the maximum values for each action
+    for action in actions_without_action:
+        if action not in maximum:
+            maximum[action] = 0
+        values = df_dict[action]
+        if values is None:
+            continue
+        # remove NaN values
+        values = [v for v in values if not pd.isna(v)]
+        max_value = max(values)
+        maximum[action] = max(maximum[action], max_value)
+        if action not in safe_maximum:
+            safe_maximum[action] = max_value
+        else:
+            safe_maximum[action] = min(safe_maximum[action], max_value)
+
+    # Save maximum to a JSON file
+    # /workspaces/plant-rl/src/environments/PlantGrowthChamber/configs/caibration.json
+    max_file_path = "/workspaces/plant-rl/src/environments/PlantGrowthChamber/configs/calibration.json"
+    with open(max_file_path, "w") as max_file:
+        out = {
+            "maximum": maximum,
+            "safe_maximum": safe_maximum,
+        }
+        json.dump(out, max_file, indent=4)
 
     # Update the calibrations in alliance-zone*.json
     alliance_zone_file = f"/workspaces/plant-rl/src/environments/PlantGrowthChamber/configs/alliance-{zone_str}.json"
