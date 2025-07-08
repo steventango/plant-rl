1ed96392f51a9ac4fedfe40f1e34f8afa7676646
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-07-08 14:01:17 +0000

    Fix DLI normalization range in get_observation method

--- src/environments/PlantGrowthChamber/TimeDLIPlantGrowthChamber.py
+++ src/environments/PlantGrowthChamber/TimeDLIPlantGrowthChamber.py
@@ -10,7 +10,7 @@ class TimeDLIPlantGrowthChamber(PlantGrowthChamber):
 
     async def get_observation(self):  # type: ignore
         epoch_time, _, df = await super().get_observation()
-        dli = np.clip(normalize(self.dli, 0, 66), 0, 1)
+        dli = np.clip(normalize(self.dli, 0, 660), 0, 1)
         local_time = epoch_time.astimezone(self.tz)
         hours_since_start = (local_time.hour - 9) + ((local_time.minute - 30) / 60)
         hours_normalized = np.clip(hours_since_start / 11.0, 0, 1)
