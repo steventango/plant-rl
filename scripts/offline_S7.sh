dd5c7405e3555a0ec464a3a108b1aab0a63f12b9
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-07-08 03:59:10 +0000

    Add commands for both BatchESARSA experiments

--- scripts/offline_S7.sh
+++ scripts/offline_S7.sh
@@ -3,4 +3,6 @@
 # exit script on error
 set -e
 
-python src/main_offline.py -e experiments/offline/S7/P0/BatchESARSA.json -i 0
+python src/main_offline.py -e experiments/offline/S7/P0/BatchESARSA.json -i 0 &
+python src/main_offline.py -e experiments/offline/S7/P1/BatchESARSA.json -i 0 &
+wait
