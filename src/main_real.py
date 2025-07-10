002cee888e59e1ae0147d8e16eb7113e04d649e2
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-07-10 18:46:27 +0000

    Refactor main function to put most of the code under try/catch

--- src/main_real.py
+++ src/main_real.py
@@ -27,95 +27,47 @@ from utils.plotting import plot_q_values_and_diff
 from utils.preempt import TimeoutHandler
 from utils.RlGlue.rl_glue import AsyncRLGlue
 
-# ------------------
-# -- Command Args --
-# ------------------
-parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--exp", type=str, required=True)
-parser.add_argument("-i", "--idxs", nargs="+", type=int, required=True)
-parser.add_argument("--save_path", type=str, default="./")
-parser.add_argument("--checkpoint_path", type=str, default="./checkpoints/")
-parser.add_argument("--silent", action="store_true", default=False)
-parser.add_argument("--gpu", action="store_true", default=False)
-parser.add_argument(
-    "-d",
-    "--deploy",
-    action="store_true",
-    default=False,
-    help="Allows for easily restarting logging for crashed runs in deployment. If the run already exists, then wandb will resume logging to the same run.",
-)
-
-args = parser.parse_args()
-
-# ---------------------------
-# -- Library Configuration --
-# ---------------------------
-
-
-device = "gpu" if args.gpu else "cpu"
-jax.config.update("jax_platform_name", device)
-
-logging.basicConfig(
-    level=logging.ERROR,
-    format="[%(asctime)s] %(levelname)s:%(name)s %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
-logger = logging.getLogger("exp")
-prod = "cdr" in socket.gethostname() or args.silent
-if not prod:
-    logger.setLevel(logging.DEBUG)
-
-# ----------------------
-# -- Experiment Def'n --
-# ----------------------
-timeout_handler = TimeoutHandler()
-
-exp = ExperimentModel.load(args.exp)
-indices = args.idxs
-
-Problem = getProblem(exp.problem)
+logger = logging.getLogger(__name__)
 
 
 async def main():
-    for idx in indices:
-        chk = Checkpoint(exp, idx, base_path=args.checkpoint_path)
-        chk.load_if_exists()
-        timeout_handler.before_cancel(chk.save)
+    # ------------------
+    # -- Command Args --
+    # ------------------
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--exp", type=str, required=True)
+    parser.add_argument("-i", "--idxs", nargs="+", type=int, required=True)
+    parser.add_argument("--save_path", type=str, default="./")
+    parser.add_argument("--checkpoint_path", type=str, default="./checkpoints/")
+    parser.add_argument("--silent", action="store_true", default=False)
+    parser.add_argument("--gpu", action="store_true", default=False)
+    parser.add_argument(
+        "-d",
+        "--deploy",
+        action="store_true",
+        default=False,
+        help="Allows for easily restarting logging for crashed runs in deployment. If the run already exists, then wandb will resume logging to the same run.",
+    )
 
-        run = exp.getRun(idx)
+    args = parser.parse_args()
 
-        # set random seeds accordingly
-        np.random.seed(run)
+    # ---------------------------
+    # -- Library Configuration --
+    # ---------------------------
 
-        # build stateful things and attach to checkpoint
-        problem = chk.build("p", lambda: Problem(exp, idx, None))
-        agent = chk.build("a", problem.getAgent)
-        env = chk.build("e", problem.getEnvironment)
+    device = "gpu" if args.gpu else "cpu"
+    jax.config.update("jax_platform_name", device)
 
-        context = exp.buildSaveContext(idx, base=args.save_path)
-        agent_path = Path(context.resolve()).relative_to("results")
-        dataset_path = Path("/data") / agent_path / env.zone.identifier
-        images_save_keys = problem.exp_params.get("image_save_keys")
+    logging.basicConfig(
+        level=logging.ERROR,
+        format="[%(asctime)s] %(levelname)s:%(name)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    prod = "cdr" in socket.gethostname() or args.silent
+    if not prod:
+        logger.setLevel(logging.DEBUG)
 
-        glue = chk.build(
-            "glue", lambda: AsyncRLGlue(agent, env, dataset_path, images_save_keys)
-        )
-        chk.initial_value("episode", 0)
-
-        load_params = problem.exp_params.get("load", None)
-        if isinstance(load_params, dict):
-            loaded_chk = Checkpoint(
-                exp, run, base_path=args.checkpoint_path, load_path=load_params["path"]
-            )
-            loaded_chk.load()
-            chk.load_from_checkpoint(loaded_chk, load_params.get("config"))
-
-        config = {
-            **problem.params,
-            "context": str(agent_path),
-            "zone": serialize_zone(env.zone),
-        }
-
+    for idx in args.idxs:
         if args.deploy:
             run_id = args.exp.replace("/", "-").removesuffix(".json")
             resume = "allow"
@@ -132,8 +84,7 @@ async def main():
             project="main",
             id=run_id,
             resume=resume,
-            notes=str(agent_path),
-            config=config,
+            config={},
             settings=wandb.Settings(
                 x_stats_disk_paths=(
                     "/",
@@ -146,14 +97,76 @@ async def main():
         handler = WandbAlertHandler(wandb_run)
         logger.addHandler(handler)
 
-        # save config to dataset
-        if not dataset_path.exists():
-            dataset_path.mkdir(parents=True, exist_ok=True)
-        config_path = dataset_path / "config.json"
-        with open(config_path, "w") as f:
-            json.dump(config, f, indent=4)
+        env = None
+        chk = None
 
         try:
+            # ----------------------
+            # -- Experiment Def'n --
+            # ----------------------
+            timeout_handler = TimeoutHandler()
+            exp = ExperimentModel.load(args.exp)
+
+            Problem = getProblem(exp.problem)
+
+            chk = Checkpoint(exp, idx, base_path=args.checkpoint_path)
+            chk.load_if_exists()
+            timeout_handler.before_cancel(chk.save)
+
+            run = exp.getRun(idx)
+
+            # set random seeds accordingly
+            np.random.seed(run)
+
+            # build stateful things and attach to checkpoint
+            problem = chk.build("p", lambda: Problem(exp, idx, None))
+            agent = chk.build("a", problem.getAgent)
+            env = chk.build("e", problem.getEnvironment)
+
+            context = exp.buildSaveContext(idx, base=args.save_path)
+            agent_path = Path(context.resolve()).relative_to("results")
+            dataset_path = Path("/data") / agent_path / env.zone.identifier
+            images_save_keys = problem.exp_params.get("image_save_keys")
+
+            config = {
+                **problem.params,
+                "context": str(agent_path),
+                "zone": serialize_zone(env.zone),
+            }
+            wandb_run.config.update(config)
+
+            def glue_builder():
+                assert env is not None, (
+                    "Environment must be initialized before creating glue."
+                )
+                return AsyncRLGlue(
+                    agent,
+                    env,
+                    dataset_path,
+                    images_save_keys=images_save_keys,
+                )
+
+            glue = chk.build("glue", glue_builder)
+            chk.initial_value("episode", 0)
+
+            load_params = problem.exp_params.get("load", None)
+            if isinstance(load_params, dict):
+                loaded_chk = Checkpoint(
+                    exp,
+                    run,
+                    base_path=args.checkpoint_path,
+                    load_path=load_params["path"],
+                )
+                loaded_chk.load()
+                chk.load_from_checkpoint(loaded_chk, load_params.get("config"))
+
+            # save config to dataset
+            if not dataset_path.exists():
+                dataset_path.mkdir(parents=True, exist_ok=True)
+            config_path = dataset_path / "config.json"
+            with open(config_path, "w") as f:
+                json.dump(config, f, indent=4)
+
             # Run the experiment
             start_time = time.time()
             is_mock_env = exp.problem.startswith("Mock")
@@ -266,8 +279,10 @@ async def main():
             # ------------
             # -- Saving --
             # ------------
-            await env.close()
-            chk.save()
+            if env is not None and hasattr(env, "close"):
+                await env.close()
+            if chk is not None:
+                chk.save()
         wandb_run.finish()
 
 
