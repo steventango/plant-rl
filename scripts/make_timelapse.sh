d6d8e9024ced7ec0abb7ad5503e296cc4688368c
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-07-22 04:38:59 +0000

    Make timelapses for E10/P1

--- scripts/make_timelapse.sh
+++ scripts/make_timelapse.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-ffmpeg -framerate 24 -pattern_type glob -i  '/data/nazmus_exp/z11c1/processed/v3.6.0/images/*_stacked.jpg' -c:v libx264 -pix_fmt yuv420p timelapse_nazmusz11c1.mp4
+yes | ffmpeg -framerate 24 -pattern_type glob -i  '/data/online/E10/P1/Constant10/alliance-zone10/processed/v3.6.1/images/*_annotated.jpg' -c:v libx264 -pix_fmt yuv420p /data/online/E10/P1/Constant10/alliance-zone10/processed/v3.6.1/timelapse.mp4 &
+yes | ffmpeg -framerate 24 -pattern_type glob -i  '/data/online/E10/P1/Poisson1/alliance-zone01/processed/v3.6.1/images/*_annotated.jpg' -c:v libx264 -pix_fmt yuv420p /data/online/E10/P1/Poisson1/alliance-zone01/processed/v3.6.1/timelapse.mp4 &
+wait
 
 # maybe need density area to help filter out bad masks
