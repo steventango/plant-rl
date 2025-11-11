1aa9e3169cdfc74482c8f1bad61b379ce1fc9833
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-11-11 17:08:20 +0000

    Use 3D action space

--- src/problems/GPsim.py
+++ src/problems/GPsim.py
@@ -8,7 +8,7 @@ from problems.BaseProblem import BaseProblem
 class GPsim(BaseProblem):
     def __init__(self, exp: ExperimentModel, idx: int, collector: Collector):
         super().__init__(exp, idx, collector)
-        self.actions = 6
+        self.actions = 3
         self.observations = (5,)
         self.gamma = 0.99
         self.env = GPsim_1day(**self.env_params)
