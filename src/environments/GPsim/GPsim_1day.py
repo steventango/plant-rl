a7953533e3aec0e094a8ad42a1e43bb0120f9346
Anffany Chen <anffany@ualberta.ca>
2025-11-11 00:19:54 -0700

    retrained gp model with beta=0.9, upgraded gpjax, minor changes

--- src/environments/GPsim/GPsim_1day.py
+++ src/environments/GPsim/GPsim_1day.py
@@ -12,8 +12,8 @@ logger.setLevel(logging.DEBUG)
 
 class GPsim_1day(BaseEnvironment):
     def __init__(self, stochastic_pred=False, optimism=0, episode_length=13, **kwargs):
-        self.state_dim = (1,)
-        self.current_state = np.empty(1)
+        self.state_dim = (5,)
+        self.current_state = np.empty(5)
         self.action_dim = 6
         self.num_steps = 0
         self.gamma = 0.99
@@ -24,15 +24,14 @@ class GPsim_1day(BaseEnvironment):
 
         data_path = (
             os.path.dirname(os.path.abspath(__file__))
-            + "/models/E13only_every_day+size_1day_trace5_ratio.pickle"
+            + "/models/E13only_every_day+size_1day_trace9_ratio.pickle"
         )
         with open(data_path, "rb") as f:
             self.GP_model = pickle.load(f)
 
-    def get_observation(self, action):
-        trace5 = [self.action_trace5[i].compute().item() for i in range(3)]
+    def get_observation(self, action, trace):
         input = np.vstack(
-            [np.hstack([[self.num_steps, self.current_area], action, trace5])]
+            [np.hstack([[self.num_steps, self.current_area], action, trace])]
         )
 
         if not self.stochastic_pred:
@@ -51,10 +50,11 @@ class GPsim_1day(BaseEnvironment):
 
     def start(self):
         self.num_steps = 0
-        self.action_trace5 = [uema(alpha=0.5), uema(alpha=0.5), uema(alpha=0.5)]
+        self.action_trace9 = [uema(alpha=0.1), uema(alpha=0.1), uema(alpha=0.1)]
         self.current_area = np.random.uniform(30, 90)
-        self.current_state = np.array([self.current_area])
-
+        self.current_state = np.array(
+            [self.num_steps / 14, self.normalize_area(self.current_area), 0, 0, 0]
+        )
         return self.current_state, self.get_info()
 
     def step(self, action: np.ndarray):  # type:ignore
@@ -65,14 +65,17 @@ class GPsim_1day(BaseEnvironment):
 
         # Update action traces
         for i in range(3):
-            self.action_trace5[i].update(action_rwb[i])
+            self.action_trace9[i].update(action_rwb[i])
+        trace9 = [self.action_trace9[i].compute().item() for i in range(3)]
 
-        next_area = self.get_observation(action_rwb)
+        next_area = self.get_observation(action_rwb, trace9)
 
         reward = next_area / self.current_area - 1
 
         self.current_area = next_area
-        self.current_state = np.array([self.current_area])
+        self.current_state = np.array(
+            [self.num_steps / 14, self.normalize_area(self.current_area)] + trace9
+        )
 
         if self.num_steps == self.episode_length:
             return reward, self.current_state, True, self.get_info()
@@ -103,3 +106,6 @@ class GPsim_1day(BaseEnvironment):
         # This finds coefficients that minimize ||action - basis @ coefficients||^2
         coefficients, residuals, rank, s = np.linalg.lstsq(basis, action, rcond=None)
         return coefficients
+
+    def normalize_area(self, area, min_area=14.3125, max_area=1211.0):
+        return (area - min_area) / (max_area - min_area)
