5ffccd7ee610c495f50c5f2b83d755fe2bbf9416
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-11-12 01:52:28 +0000

    Run eval and plots too

--- run.sh
+++ run.sh
@@ -11,3 +11,15 @@ python src/main.py -e experiments/offline/S8/P0/InAC_GPsim0.5.json -i 0 &
 python src/main.py -e experiments/offline/S8/P0/InAC_GPsim0.75.json -i 0 &
 python src/main.py -e experiments/offline/S8/P0/InAC_GPsim1.json -i 0 &
 wait
+python src/main.py -e experiments/offline/S8/P0/InAC_GPsim0_eval.json -i 0 &
+python src/main.py -e experiments/offline/S8/P0/InAC_GPsim0.25_eval.json -i 0 &
+python src/main.py -e experiments/offline/S8/P0/InAC_GPsim0.5_eval.json -i 0 &
+python src/main.py -e experiments/offline/S8/P0/InAC_GPsim0.75_eval.json -i 0 &
+python src/main.py -e experiments/offline/S8/P0/InAC_GPsim1_eval.json -i 0 &
+wait
+python src/visualize_simplex_policy.py --exp_path results/offline/S8/P0/InAC_GPsim0/0 &
+python src/visualize_simplex_policy.py --exp_path results/offline/S8/P0/InAC_GPsim0.25/0 &
+python src/visualize_simplex_policy.py --exp_path results/offline/S8/P0/InAC_GPsim0.5/0 &
+python src/visualize_simplex_policy.py --exp_path results/offline/S8/P0/InAC_GPsim0.75/0 &
+python src/visualize_simplex_policy.py --exp_path results/offline/S8/P0/InAC_GPsim1/0 &
+wait
