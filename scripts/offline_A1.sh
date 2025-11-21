eea52719c07075975d5f36cc941b526b06db78fc
Anffany Chen <anffany@ualberta.ca>
2025-11-21 13:35:39 -0700

    set up exp for training InAC in updated GPsim

--- scripts/offline_A1.sh
+++ scripts/offline_A1.sh
@@ -3,4 +3,5 @@
 # exit script on error
 set -e
 
-python scripts/local.py --runs 5 --cpus 1 -e experiments/offline/A1/RandomAgent_GPsim.json
+#python scripts/local.py --runs 5 --cpus 1 -e experiments/offline/A1/RandomAgent_GPsim.json
+python scripts/local.py --runs 5 --cpus 1 -e experiments/offline/A1/InAC_GPsim0.json
