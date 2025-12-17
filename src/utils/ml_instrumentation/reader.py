ba8aed37f2e2a1b1337d6274d5976fd7426e5b28
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-12-16 19:21:51 -0700

    fix: only use data

--- src/utils/ml_instrumentation/reader.py
+++ src/utils/ml_instrumentation/reader.py
@@ -1,27 +1,10 @@
 from pathlib import Path
 from typing import Any
-import traceback
-import connectorx as cx
-
-import utils.ml_instrumentation._utils.sqlite as sqlu
 
 
 def get_run_ids(
     db_path: str | Path, params: dict[str, Any], data_path: str | Path | None = None
 ):
-    constraints = " AND ".join(
-        f"[{k}]={sqlu.maybe_quote(v)}" for k, v in params.items()
-    )
-
-    query = f"SELECT id FROM _metadata_ WHERE {constraints}"
-    try:
-        return cx.read_sql(f"sqlite://{db_path}", query, return_type="polars")[
-            "id"
-        ].to_list()
-    except BaseException:
-        traceback.print_exc()
-        print("Error occurred while fetching run IDs from DB")
-
     if data_path is None:
         return []
 
