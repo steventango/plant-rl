b3099e9b486f0708d214f39eccb95a824fa969a9
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-12-16 17:39:34 -0700

    feat: script for copying data to vulcan

---
+++ experiments/offline/S8/P2/sync.sh
@@ -0,0 +1,3 @@
+rsync -azP /data/plant-rl/offline/v20/normalization-stats-v20.json vulcan:/home/stang5/projects/aip-amw8/shared/plant/offline
+rsync -azP ~/.minari/datasets/plant-data/mixed-v20 vulcan:/home/stang5/.minari/datasets/plant-data/mixed-v20
+rsync -azP ~/.minari/datasets/plant-data/mixed-all-v20 vulcan:/home/stang5/.minari/datasets/plant-data/mixed-all-v20
