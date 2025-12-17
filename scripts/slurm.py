173884099510fdfe32f7cc7f7260b127abaaab69
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-12-16 18:36:32 -0700

    fix: disable wandb

--- scripts/slurm.py
+++ scripts/slurm.py
@@ -80,6 +80,7 @@ export OMP_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export NPROC=1
+export WANDB_MODE=disabled
 export XLA_FLAGS="--xla_cpu_multi_thread_eigen=false intra_op_parallelism_threads=1"
 export XLA_PYTHON_CLIENT_MEM_FRACTION={max_xla_python_client_mem_fraction / jobs}
 {device_str}
