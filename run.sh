7aa6ce02143cc9a50dad20124aa36790b05cbf26
Steven Tang <18170455+steventango@users.noreply.github.com>
2026-02-03 15:36:58 -0700

    feat: retrain with correct normalization order

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ export MKL_NUM_THREADS=1
 export NPROC=1
 export XLA_FLAGS=--xla_cpu_multi_thread_eigen=false intra_op_parallelism_threads
 
-python scripts/local.py --runs 1 -e experiments/offline/S8/P2/InAC_LN.json --entry src/main_offline.py --gpu
-python scripts/local.py --runs 1 -e experiments/offline/S8/P2/Constant_White.json --entry src/main_offline.py --gpu
-python scripts/local.py --runs 1 -e experiments/offline/S8/P2/Constant_Red.json --entry src/main_offline.py --gpu
-python scripts/local.py --runs 1 -e experiments/offline/S8/P2/Constant_Blue.json --entry src/main_offline.py --gpu
-python scripts/local.py --runs 1 -e experiments/offline/S8/P2/Dirichlet_Uniform.json --entry src/main_offline.py --gpu
+python scripts/local.py --runs 1 -e experiments/offline/S8/P3/InAC_LN.json --entry src/main_offline.py --gpu
+python scripts/local.py --runs 1 -e experiments/offline/S8/P3/Constant_White.json --entry src/main_offline.py --gpu
+python scripts/local.py --runs 1 -e experiments/offline/S8/P3/Constant_Red.json --entry src/main_offline.py --gpu
+python scripts/local.py --runs 1 -e experiments/offline/S8/P3/Constant_Blue.json --entry src/main_offline.py --gpu
+python scripts/local.py --runs 1 -e experiments/offline/S8/P3/Dirichlet_Uniform.json --entry src/main_offline.py --gpu
