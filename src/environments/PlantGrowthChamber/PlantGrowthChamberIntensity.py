5af9c7d242e9d5bf7f540ce7d841689b8a671673
Anffany Chen <anffany@ualberta.ca>
2026-05-22 14:44:08 -0600

    add logging to agent's scalar action

--- src/environments/PlantGrowthChamber/PlantGrowthChamberIntensity.py
+++ src/environments/PlantGrowthChamber/PlantGrowthChamberIntensity.py
@@ -9,6 +9,7 @@ class PlantGrowthChamberIntensity(PlantGrowthChamber):
         super().__init__(**kwargs)
         self.min_ppfd = min_ppfd
         self.max_ppfd = max_ppfd
+        self.agent_action = None
 
     async def get_observation(self):  # type: ignore
         epoch_time, _, df = await super().get_observation()
@@ -23,7 +24,31 @@ class PlantGrowthChamberIntensity(PlantGrowthChamber):
 
     async def step(self, action: float | np.ndarray):
         if isinstance(action, np.ndarray):
+            self.agent_action = None
             return await super().step(action)
-        # map scalar action in [-1, 1] to ppfd in [min_ppfd, max_ppfd]
-        ppfd = self.min_ppfd + (action + 1) / 2 * (self.max_ppfd - self.min_ppfd)
+        
+        self.agent_action = action
+        ppfd = self.min_ppfd + (action + 1) / 2 * (self.max_ppfd - self.min_ppfd)  # map scalar action in [-1, 1] to ppfd in [min_ppfd, max_ppfd]
         return await super().step(BALANCED_ACTION_100 * ppfd / 100)
+
+    def get_info(self):
+        if self.df.empty:
+            return {
+                "df": self.df,
+                "env_time": self.time.timestamp(),
+                "agent_action": self.agent_action,
+            }
+        mean = np.array(
+            [self.uema_areas[i].compute() for i in range(self.zone.num_plants)]
+        ).flatten()
+        upper = mean * (1 + self.clean_area_upper)
+        lower = mean * (1 - self.clean_area_lower)
+        return {
+            "df": self.df,
+            "mean_clean_area": np.mean(self.clean_areas[-1]),
+            "uema_area": mean,
+            "upper_area": upper,
+            "lower_area": lower,
+            "env_time": self.time.timestamp(),
+            "agent_action": self.agent_action,
+        }
