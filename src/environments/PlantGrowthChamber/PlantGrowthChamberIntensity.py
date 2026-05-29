c69ae0ecb7c7deea1cc6123aca8172d6fccf1b33
Anffany Chen <anffanychen@gmail.com>
2026-05-29 15:11:38 -0600

    minor fixes

--- src/environments/PlantGrowthChamber/PlantGrowthChamberIntensity.py
+++ src/environments/PlantGrowthChamber/PlantGrowthChamberIntensity.py
@@ -13,7 +13,13 @@ class PlantGrowthChamberIntensity(PlantGrowthChamber):
         epoch_time, _, df = await super().get_observation()
         local_time = epoch_time.astimezone(self.tz)
 
-        return [local_time, df["mean_clean_area"]]
+        if not df.empty:
+            plant_areas = df["clean_area"].to_numpy().flatten()
+            mean_area = float(np.mean(plant_areas)) if len(plant_areas) > 0 else 0.0
+        else:
+            mean_area = 0.0
+
+        return [local_time, mean_area]
 
     async def step(self, action: float | np.ndarray):
         if isinstance(action, np.ndarray):
