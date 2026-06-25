9611bd1fd801a7006833ae9bce4e1926cf5dce5b
Anffany Chen <anffany@ualberta.ca>
2026-06-25 11:33:09 -0600

    changed loading pretrained ppo agent to loading pretrained policy

--- src/problems/PPOPlantGrowthChamber.py
+++ src/problems/PPOPlantGrowthChamber.py
@@ -16,7 +16,7 @@ class PPOPlantGrowthChamber(BasePlantGrowthChamberAsyncProblem):
         observation : "day_area_color_trace"
         zone        : e.g. "alliance-zone01"
         timezone    : e.g. "Etc/GMT-2"
-        frozen_agent_path  : path to the .zip produced by PPO.save()
+        policy_path  : path to the learned policy in .pt format
     """
 
     def __init__(self, exp: ExperimentModel, idx: int, collector: Collector):
